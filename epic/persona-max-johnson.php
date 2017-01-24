ec862c5ae0d5d1d4b6d09ac276373b6cd70f6517
Mason Crane <masonecrane@gmail.com>
2017-01-24 16:30:01 -0700

    added valid html to persona-max-johnson

---
+++ epic/persona-max-johnson.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Persona: Max Johnson</title>
+	</head>
+	<body>
+		<p><b>Name:</b> Max Johnson</p>
+		<p><b>Age:</b> 28</p>
+		<p><b>Occupation:</b> user experience engineer for Lavu</p>
+		<p><b>Technology:</b> iPhone 7 running iOS 10.2 on Verizon, and a 2016 MacBook Pro 15” running OS X 10.12.2. Max chose a laptop as opposed to a desktop for mobility.</p>
+		<p><b>Attitudes & Behaviors:</b> Max loves his job, but he works a lot, and doesn’t have the time to network and book gigs. He grew up making music in various forms, and he enjoys the relaxing nature of writing music and performing. Max is an avid fan of social media, but has trouble using his online persona to make the connections he needs to make in order to book gigs on a regular basis.</p>
+		<p><b>Frustrations & Needs:</b> Max is frustrated, because he has a ton of good material, but a lack of connections to utilize to book gigs on a regular basis. After being at a desk, behind a screen for 50+ hours per week he just wants to relax, perform his music and meet cool people. Max needs an easy way to connect with the right people in town so that he can book regular gigs, get his music in front of more people, and connect with his current and future fans.
+			Goals: Max wants to share his music with as many people as possible, and with his limited time he needs an easy, quick way to show off his talents and connect with decision makers in order to land more gigs.</p>
+		<p><b>Use Case:</b> It’s 4:48pm and Max’s co-worker, Kelly, just told him about this new website; gighub. Max is intrigued, because he thinks this may be the answer to his lack of time and connections. Instead of working past 5pm, he decides to stay at work, but off the clock, and go to gighub. The succinct tag line on the front page of gighub immediately resonates with Max, so he clicks the sign up button and uses oAuth to connect his gighub account to his twitter account: @music2themax. Max then uploads a profile picture, fills out any remaining data in his profile, and begins to search for venues.</p>
+	</body>
+</html>

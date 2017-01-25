546a1d91c439fd199e18e2ea08fc38a6b10927f3
Mason Crane <masonecrane@gmail.com>
2017-01-25 13:36:35 -0700

    fixed validation error with nesting an <ol> within a <p>

--- epic/persona-max-johnson.php
+++ epic/persona-max-johnson.php
@@ -20,7 +20,7 @@
 			<p><strong>Frustrations & Needs:</strong> Max is frustrated, because he has a ton of good material, but a lack of connections to utilize to book gigs on a regular basis. After being at a desk, behind a screen for 50+ hours per week he just wants to relax, perform his music and meet cool people. Max needs an easy way to connect with the right people in town so that he can book regular gigs, get his music in front of more people, and connect with his current and future fans.</p>
 			<p><strong>Goals:</strong> Max wants to share his music with as many people as possible, and with his limited time he needs an easy, quick way to show off his talents and connect with decision makers in order to land more gigs.</p>
 			<p><strong>Use Case:</strong> It’s 4:48pm and Max’s co-worker, Kelly, just told him about this new website; gighub. Max is intrigued, because he thinks this may be the answer to his lack of time and connections. Instead of working past 5pm, he decides to stay at work, but off the clock, and go to gighub. The succinct tag line on the front page of gighub immediately resonates with Max, so he clicks the sign up button and uses oAuth to connect his gighub account to his twitter account: @music2themax. Max then uploads a profile picture, fills out any remaining data in his profile, and begins to search for venues.</p>
-			<p><strong>Interaction Flow:</strong>
+			<p><strong>Interaction Flow:</strong></p>
 				<ol>
 					<li>Click on profile button in upper right corner of screen</li>
 					<li>Click on edit profile button</li>
@@ -34,7 +34,6 @@
 					<li>Return to personal profile page</li>
 					<li>Click into search bar and begin searching for venues</li>
 				</ol>
-			</p>
 		</main>
 	</body>
 </html>

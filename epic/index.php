0225b9dc582c8db9a1e8e63cfdb3a10ed8fe0841
Mason Crane <masonecrane@gmail.com>
2017-01-25 13:23:33 -0700

    filled in some info and links in index.php, created user-stories.php

---
+++ epic/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+		      content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>EPIC Index!</title>
+	</head>
+	<body>
+		<header>
+			<h1>Let the links commence</h1>
+		</header>
+		<main>
+			<h2>System Summary</h2>
+			<p>A place for local venues (bars, dances, concert halls, weddings, birthday parties for clowns and shit) to find acts to perform.  A place for artists and acts to create a “gigfolio” for venues to view and book. When the contract is finalized by the gigger and the giggee the event will be viewable by the fan base in a calendar format. </p>
+			<h2>Persona files</h2>
+			<p><a href="persona-four18-bar.php">Venue Persona</a></p>
+			<p><a href="persona-max-johnson.php">Musician Persona</a></p>
+			<p><a href="persona-david-hampton.php">Fan Persona</a></p>
+			<p><a href="persona-chris-angle.php">Organizer Persona</a></p>
+			<h2>User Stories</h2>
+			<p><a href="user-stories.php">User Stories</a></p>
+		</main>
+	</body>
+</html>

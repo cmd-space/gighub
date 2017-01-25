f43f950bb4cef29c4f202ee3a585acff83bcfce6
Brandon Steider <bsteider@cnm.edu>
2017-01-24 20:13:45 -0700

    Added persona to David Hampton

---
+++ epic/persona-david-hampton.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Persona: David Hampton</title>
+	</head>
+	<body>
+		<p><strong>Name:</strong> David Hampton</p>
+		<p><strong>Age:</strong> 18</p>
+		<p><strong>Occupation:</strong> Works as a busser at a restaurant, and being angsty in this senior year of high school. </p>
+		<p><strong>Technology:</strong> Iphone 7 with a belt holder that also holds a sweater vest. He uses an Asus laptop with Windows 10.
+		</p>
+		<p><strong>Attitudes & Behaviors:</strong>  Often shows up late, doesn’t put a lot of effort into tasks, he is the definition of edgy angsty teen. In his spare time he watches independent movies on Netflix, and writes stories (children's books, novels, raps, poetry).
+		</p>
+		<p><strong>Frustrations & Needs:</strong> When he’s blowing off homework, he looks for concerts or other shows to go to. Typically he listens to indie rock, but has a soft side for heavy metal and EDM. He would like to find somewhere to perform his rap music, or maybe an open mic night for his poetry. It’s frustrating how hard it is to find out about these events around town. Using gighub he would have an easy time finding any venue for any type of show his little heart demands.</p>
+
+		<p><strong>Use Case:</strong> David would likely use gighub to find shows around town to attend and listen to, or possibly find a place where he could perform his raps or poetry to a crowd. He needs the best possible option to discover local (someday countrywide, or dream big and go worldwide) venues with shows, what time the shows are, what kind of show it is, and other general information.</p>
+	</body>
+</html>

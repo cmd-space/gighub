699c01877cb50a2918f8834eacc50fce9835e2aa
Mason Crane <masonecrane@gmail.com>
2017-01-26 16:38:37 -0700

    fleshed out conceptual-model.php

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -16,15 +16,17 @@
 				<li>Profile
 				   <ol>
 					   <li>profileId(PK)</li>
-					   <!--	would a FK go here to connect to oAuth entity, since oAuth technically happens before a profile is created?-->
-					   <li>profileUser</li>
+					   <li>profileOAuthId(FK?)</li>
+					   <li>profileUserName</li>
 					   <li>profileLocation</li>
 					   <li>profileBio</li>
 					   <li>profileUrl</li>
-					   <li>profileSoundcloudUser</li>
+					   <li>profileSoundCloudUser</li>
+						<li>profileImageCloudinaryId</li>
+						<li>profileType</li>
 				   </ol>
 				</li>
-				<li>oAuth
+				<li>OAuth
 				   <ol>
 					   <li>oAuthId(PK)</li>
 					   <li>oAuthProfileId(FK)</li>
@@ -32,6 +34,46 @@
 					   <li>oAuthTwitterId</li>
 				   </ol>
 				</li>
+				<li>Post
+					<ol>
+						<li>postId(PK)</li>
+						<li>postProfileId(FK)</li>
+						<li>postTitle</li>
+						<li>postLocation</li>
+						<li>postContent</li>
+						<!-- postGoogleMap is still not decided -->
+						<li>postGoogleMap</li>
+						<li>postEventDate</li>
+						<li>postCreatedDate</li>
+						<li>postImageCloudinaryId</li>
+					</ol>
+				</li>
+				<li>Tag
+					<ol>
+						<li>tagId(PK)</li>
+						<li>tagContent</li>
+					</ol>
+				</li>
+				<li>Post Tag
+					<ol>
+						<li>postTagTagId(FK)</li>
+						<li>postTagPostId(FK)</li>
+					</ol>
+				</li>
+				<li>Profile Tag
+					<ol>
+						<li>profileTagTagId(FK)</li>
+						<li>profileTagProfileId(FK)</li>
+					</ol>
+				</li>
+<!--				<li>Profile Image-->
+<!--					<ol>-->
+<!--						<li>profileImageId(PK)</li>-->
+<!--						<li>profileImageProfileId(FK)</li>-->
+<!--						<li>profileImageUploadFileName</li>-->
+<!--						<li>profileImageUrl</li>-->
+<!--					</ol>-->
+<!--				</li>-->
 			</ul>
 		</main>
 	</body>

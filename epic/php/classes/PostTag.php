16cb084ee1b60819c1927a9c028fca2eda261d34
Brandon Steider <bsteider@cnm.edu>
2017-01-31 16:54:21 -0700

    making progress through PostTag https://app.asana.com/0/256424297438824/258096775823636

--- epic/php/classes/PostTag.php
+++ epic/php/classes/PostTag.php
@@ -1,2 +1,35 @@
-</php
+<?php
 namespace Edu\Cnm\Bsteider\PostTag;
+
+require_once("autoload.php");
+
+/**
+ * @author Brandon Steider <bsteider@cnm.edu>
+ * @version 3.0.0
+ **/
+class PostTag implements \JsonSerializable {
+	use ValidateDate;
+	/**
+	 * id for this PostTag; this is a foreign key
+	 * @var int $postTagId
+	 **/
+	private $postTagId;
+	/**
+	 * id for this Post Tag; this is a foreign key
+	 * @var int $PostTagId
+	 **/
+	private $PostTagTagId
+	/**
+	 * actual textual content of this PostTag
+	 * @var \DateTime $PostTagDate
+	 **/
+	private $postTagPostId;
+	/**
+	 * actual textual content of this PostTag
+	 * @var \DateTime $postTagPostId
+	 **/
+	private $PostTagDate;
+
+public function __construct()
+}
+

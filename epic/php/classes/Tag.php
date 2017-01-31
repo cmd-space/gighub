e80b303a9667c95d5fa9aacb0796b68714c3ae99
Dante Conley <danteconley@icloud.com>
2017-01-31 09:58:53 -0700

    started constructor on tagclass
    https://app.asana.com/0/256424297438824/258096775823628

--- epic/php/classes/Tag.php
+++ epic/php/classes/Tag.php
@@ -12,5 +12,27 @@ require_once("autoload.php");
  **/
 
 Class CreateTag implements \JsonSerializable {
-private
+	/**
+	 * id for this Tag; this is the primary key
+	 * @var int $tagId
+	 **/
+	private $tagId;
+	/**
+	 *textual content of this tag
+	 * @var string $tagContent;
+	 **/
+	private $tagContent;
+
+	/**
+	 * constructor for this Tag
+	 *
+	 * @param int|null $newTagId id of this Tag or null if a new Tag
+	 * @param string $newTagContent string containing actual tag data
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (eg strings too long, negative integers)
+	 * @throw \TypeError if data types violate hints
+	 * @throws \Exception if some other exception occurs
+	 **/HEY YOU NEED TO FINISH SHIT HERE ASSHOLE!
+
+
 }

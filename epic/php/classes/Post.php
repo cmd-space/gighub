cf7032c2b4a3cbe2c122c6148bcbae2bb40a8b32
Joseph Ramirez <jramirez98@cnm.edu>
2017-01-31 14:58:39 -0700

    added the accessors and mutators of the postId, postProfileId, PostVenueId, and postContentId https://app.asana.com/0/256424297438824/258096775823626

--- epic/php/classes/Post.php
+++ epic/php/classes/Post.php
@@ -59,7 +59,7 @@ class Post implements \JsonSerializable {
 	 * @return int|null value of post id
 	 */
 	public function getPostId() {
-		return $this->postId;
+		return ($this->postId);
 	}
 	/**
 	 * mutator method for post id
@@ -83,5 +83,92 @@ class Post implements \JsonSerializable {
 		//convert and store the post id
 		$this->postId = $newPostId;
 	}
+
+	/**
+	 * accessor method for post profile id
+	 *
+	 * @return int value of post profile id
+	 */
+	public function getPostProfileId() {
+		return($this->postProfileId);
+	}
+
+	/**
+	 * mutator method for post profile id
+	 *
+	 * @param int $newPostProfileId new value of tweet profile id
+	 * @throws \RangeException if $newProfileId is not positive
+	 * @throws \TypeError if $newProfileId is not an integer
+	 */
+	public function setPostProfileId(int $newPostProfileId) {
+
+		//verify the profile id is positive
+		if($newPostProfileId<= 0) {
+			throw(new \RangeException("do you know what you are doing?"));
+		}
+
+		// convert and store the post profile id
+		$this->postProfileId = $newPostProfileId;
+	}
+
+	/**
+	 * accessor method for post venue id
+	 *
+	 * @return int value of post venue id
+	 **/
+	public function getPostVenueId() {
+		return($this->postVenueId);
+	}
+
+	/**
+	 * mutator method for post venue Id
+	 *
+	 * @param int $newPostVenueId new value of post venue id
+	 * @throws \RangeException if $newVenueId is not positive
+	 * @throws \TypeError if $newVenueId is not an integer
+	 */
+	public function setPostVenueId(int $newPostVenueId) {
+
+		//verify the venue id is positive
+		if($newPostVenueId <= 0) {
+			throw(new \RangeException("i don't think you know what you are doing"));
+		}
+		//convert and store the venue id
+		$this->postVenueId = $newPostVenueId;
+	}
+
+	/**
+	 * accessor method for post content
+	 *
+	 * @return string value of post content
+	 */
+	public function getPostContent() {
+		return($this->postContent);
+	}
+
+	/**
+	 * mutator method for post content
+	 *
+	 * @param string $newPostContent new value of post content
+	 * @throws \InvalidArgumentException if  $newPostContent is not a string or insecure
+	 * @throws \RangeException if $newPostContent is > 140 characters
+	 * @throws \TypeError if $newPostContent is not a string
+	 */
+	public function setPostContent(string $newPostContent) {
+		//verify the post content is secure
+		$newPostContent = trim($newPostContent);
+		$newPostContent = trim($newPostContent,
+		FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+		if(empty($newPostContent) === true) {
+			throw(new \InvalidArgumentException("what are trying to say?"));
+		}
+		//verify the post content will fit in the database
+		if(strlen($newPostContent) > 140) {
+			throw(new \RangeException("your talkn to much!")):
+		}
+		//store the post content
+		$this->postContent = $newPostContent;
+	}
+
 
 }

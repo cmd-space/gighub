7514c049ac3e42821c52a636532db93f2af733f0
Brandon Steider <bsteider@cnm.edu>
2017-01-31 10:33:24 -0700

    started on constructor https://app.asana.com/0/256424297438824/258096775823634

--- epic/php/classes/ProfileTag.php
+++ epic/php/classes/ProfileTag.php
@@ -1,1 +1,26 @@
 <?php
+namespace Edu\Cnm\bsteider\ProfileTag;
+
+require_once("autoload.php");
+
+/**
+ * ask later what comments to put here regarding gighub
+ * @author Brandon Steider <bsteider@cnm.edu>
+ **/
+Class ProfileTag implements \JsonSerializable {
+	/**
+	 * id for this ProfileTag; this is the foreign key
+	 * @var int $ProfileTag
+	 **/
+	private $ProfileTagTagId;
+	/**
+	 * this is a foreign key
+	 * @var int $ProfileTagProfileId
+	 */
+	private $ProfileTagProfileId;
+	/**
+	 *constructor for this ProfileTag
+	 *
+	 *
+	 */
+}

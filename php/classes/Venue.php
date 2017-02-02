dfa436a5364065cd790b4daa19b55675585e82a9
Dante Conley <danteconley@icloud.com>
2017-02-02 16:10:38 -0700

    Venue Class PDO: Insert, Update, Delete, and Get by:venueId
    https://app.asana.com/0/256424297438824/258096775823632

--- php/classes/Venue.php
+++ php/classes/Venue.php
@@ -11,7 +11,6 @@ require_once("autoload.php");
  * @author Dante Conley <danteconley@icloud.com>
  * @version 1.0
  **/
-
 class Venue implements \JsonSerializable {
 	/**
 	 * id for this Venue; this is the primary key
@@ -71,7 +70,7 @@ class Venue implements \JsonSerializable {
 	 * @throws \Exception if some other exception occurs
 	 **/
 	public function __construct(int $newVenueId = null, int $newVenueProfileId, string $newVenueName, string $newVenueStreet1, string $newVenueStreet2, string $newVenueCity, string $newVenueState, string $newVenueZip) {
-		try{
+		try {
 			$this->setVenueId($newVenueId);
 			$this->setVenueProfileId($newVenueId);
 			$this->setVenueName($newVenueName);
@@ -94,13 +93,14 @@ class Venue implements \JsonSerializable {
 			throw(new \Exception($exception->getMessage(), 0, $exception));
 		}
 	}
+
 	/**
 	 * accessor method for venue id
 	 *
 	 * @return int|null value of venue id
 	 **/
 	public function getVenueId() {
-		return($this->venueId);
+		return ($this->venueId);
 	}
 
 	/** mutator method for venue id
@@ -117,7 +117,7 @@ class Venue implements \JsonSerializable {
 		}
 
 		// verify venue id is positive
-		if($newVenueId <=0) {
+		if($newVenueId <= 0) {
 			throw(new \RangeException("venue id is not positive"));
 		}
 
@@ -131,7 +131,7 @@ class Venue implements \JsonSerializable {
 	 * @return int value of the venue profile id
 	 **/
 	public function getVenueProfileId() {
-		return($this->venueProfileId);
+		return ($this->venueProfileId);
 	}
 
 	/**
@@ -141,7 +141,7 @@ class Venue implements \JsonSerializable {
 	 * @throws \RangeException if $newVenueProfileId is not positive
 	 * @throws \TypeError if $newVenueId is not an integer
 	 **/
-	public function  setVenueProfileId(int $newVenueProfileId) {
+	public function setVenueProfileId(int $newVenueProfileId) {
 		// verify the new profile id is positive
 		if($newVenueProfileId <= 0) {
 			throw(new \RangeException("venue profile id is not positive"));
@@ -157,7 +157,7 @@ class Venue implements \JsonSerializable {
 	 * @return string value of the venue name
 	 **/
 	public function getVenueName() {
-		return($this->venueName);
+		return ($this->venueName);
 	}
 
 	/**
@@ -191,29 +191,29 @@ class Venue implements \JsonSerializable {
 	 * @return string value of the venue street 1
 	 **/
 	public function getVenueStreet1() {
-		return($this->venueStreet1);
+		return ($this->venueStreet1);
 	}
 
-		/**
-		 * mutator method for venue street 1
-		 *
-		 * @param string $newVenueStreet1 new value of venueStreet1
-		 * @throws \InvalidArgumentException if $newVenueStreet1 is insecure
-		 * @throws \RangeException if venueStreet1 is > 64 characters
-		 * @throws \TypeError if $newVenueStreet1 is not a string
-		 **/
+	/**
+	 * mutator method for venue street 1
+	 *
+	 * @param string $newVenueStreet1 new value of venueStreet1
+	 * @throws \InvalidArgumentException if $newVenueStreet1 is insecure
+	 * @throws \RangeException if venueStreet1 is > 64 characters
+	 * @throws \TypeError if $newVenueStreet1 is not a string
+	 **/
 	public function setVenueStreet1(string $newVenueStreet1) {
-	// verify the venue street 2 is secure
-	$newVenueStreet1 = trim($newVenueStreet1);
+		// verify the venue street 2 is secure
+		$newVenueStreet1 = trim($newVenueStreet1);
 		$newVenueStreet1 = filter_var($newVenueStreet1, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if(empty($newVenueStreet1) === true) {
-		throw(new \InvalidArgumentException("venue street 1 is empty or insecure"));
+			throw(new \InvalidArgumentException("venue street 1 is empty or insecure"));
 		}
 
 		// verify the venue street 1 content will fit into the database
 		if(strlen($newVenueStreet1) > 64) {
-		throw(new \RangeException("venue street 1 content too large"));
-	}
+			throw(new \RangeException("venue street 1 content too large"));
+		}
 
 		// store the !!!! content
 		$this->venueStreet1 = $newVenueStreet1;
@@ -225,29 +225,29 @@ class Venue implements \JsonSerializable {
 	 * @return string value of the venue street 2 (if any)
 	 **/
 	public function getVenueStreet2() {
-		return($this->venueStreet2);
+		return ($this->venueStreet2);
 	}
 
-		/**
-		 * mutator method for venue street 2
-		 *
-		 * @param string $newVenueStreet2 new value of venue street 2
-		 * @throws \InvalidArgumentException if $newVenueStreet2 is insecure
-		 * @throws \RangeException if venueStreet2 is > 64 characters
-		 * @throws \TypeError if $newVenueStreet2 is not a string
-		 **/
+	/**
+	 * mutator method for venue street 2
+	 *
+	 * @param string $newVenueStreet2 new value of venue street 2
+	 * @throws \InvalidArgumentException if $newVenueStreet2 is insecure
+	 * @throws \RangeException if venueStreet2 is > 64 characters
+	 * @throws \TypeError if $newVenueStreet2 is not a string
+	 **/
 	public function setVenueStreet2(string $newVenueStreet2) {
-	// verify the venue street 2 is secure
-	$newVenueStreet2 = trim($newVenueStreet2);
+		// verify the venue street 2 is secure
+		$newVenueStreet2 = trim($newVenueStreet2);
 		$newVenueStreet2 = filter_var($newVenueStreet2, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if(empty($newVenueStreet2) === true) {
-		throw(new \InvalidArgumentException("venue street 2 is empty or insecure"));
+			throw(new \InvalidArgumentException("venue street 2 is empty or insecure"));
 		}
 
 		// verify the venue street 2 content will fit into the database
 		if(strlen($newVenueStreet2) > 64) {
-		throw(new \RangeException("venue street 2 content too large"));
-	}
+			throw(new \RangeException("venue street 2 content too large"));
+		}
 
 		// store the venueStreet2 content
 		$this->venueStreet2 = $newVenueStreet2;
@@ -259,29 +259,29 @@ class Venue implements \JsonSerializable {
 	 * @return string value of the venue city
 	 **/
 	public function getVenueCity() {
-		return($this->venueCity);
+		return ($this->venueCity);
 	}
 
-		/**
-		 * mutator method for venue city
-		 *
-		 * @param string $newVenueCity new value of venue city
-		 * @throws \InvalidArgumentException if $newVenueCity is insecure
-		 * @throws \RangeException if venueCity is > 100 characters
-		 * @throws \TypeError if $newVenueCity is not a string
-		 **/
+	/**
+	 * mutator method for venue city
+	 *
+	 * @param string $newVenueCity new value of venue city
+	 * @throws \InvalidArgumentException if $newVenueCity is insecure
+	 * @throws \RangeException if venueCity is > 100 characters
+	 * @throws \TypeError if $newVenueCity is not a string
+	 **/
 	public function setVenueCity(string $newVenueCity) {
-	// verify the venue city is secure
-	$newVenueCity = trim($newVenueCity);
+		// verify the venue city is secure
+		$newVenueCity = trim($newVenueCity);
 		$newVenueCity = filter_var($newVenueCity, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if(empty($newVenueCity) === true) {
-		throw(new \InvalidArgumentException("venue city is empty or insecure"));
+			throw(new \InvalidArgumentException("venue city is empty or insecure"));
 		}
 
 		// verify the venue city content will fit into the database
 		if(strlen($newVenueCity) > 100) {
-		throw(new \RangeException("venue city content too large"));
-	}
+			throw(new \RangeException("venue city content too large"));
+		}
 
 		// store the venue city content
 		$this->venueCity = $newVenueCity;
@@ -293,29 +293,29 @@ class Venue implements \JsonSerializable {
 	 * @return string value of the venue state
 	 **/
 	public function getVenueState() {
-		return($this->venueState);
+		return ($this->venueState);
 	}
 
-		/**
-		 * mutator method for venue state
-		 *
-		 * @param string $newVenueState new value of venue state
-		 * @throws \InvalidArgumentException if $newVenueState is insecure
-		 * @throws \RangeException if venueState is > 2 characters
-		 * @throws \TypeError if $newVenueState is not a string
-		 **/
+	/**
+	 * mutator method for venue state
+	 *
+	 * @param string $newVenueState new value of venue state
+	 * @throws \InvalidArgumentException if $newVenueState is insecure
+	 * @throws \RangeException if venueState is > 2 characters
+	 * @throws \TypeError if $newVenueState is not a string
+	 **/
 	public function setVenueState(string $newVenueState) {
-	// verify the venue state is secure
-	$newVenueState = trim($newVenueState);
+		// verify the venue state is secure
+		$newVenueState = trim($newVenueState);
 		$newVenueState = filter_var($newVenueState, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if(empty($newVenueState) === true) {
-		throw(new \InvalidArgumentException("venue state is empty or insecure"));
+			throw(new \InvalidArgumentException("venue state is empty or insecure"));
 		}
 
 		// verify the venue state content will fit into the database
 		if(strlen($newVenueState) > 2) {
-		throw(new \RangeException("venue state content too large"));
-	}
+			throw(new \RangeException("venue state content too large"));
+		}
 
 		// store the venue state content
 		$this->venueState = $newVenueState;
@@ -327,42 +327,149 @@ class Venue implements \JsonSerializable {
 	 * @return string value of the venue zip
 	 **/
 	public function getVenueZip() {
-		return($this->venueZip);
+		return ($this->venueZip);
 	}
 
-		/**
-		 * mutator method for venue zip
-		 *
-		 * @param string $newVenueZip new value of venue zip
-		 * @throws \InvalidArgumentException if $newVenueZip is insecure
-		 * @throws \RangeException if venueZip is > 10 characters
-		 * @throws \TypeError if $new!!!!!! is not a string
-		 **/
+	/**
+	 * mutator method for venue zip
+	 *
+	 * @param string $newVenueZip new value of venue zip
+	 * @throws \InvalidArgumentException if $newVenueZip is insecure
+	 * @throws \RangeException if venueZip is > 10 characters
+	 * @throws \TypeError if $new!!!!!! is not a string
+	 **/
 	public function setVenueZip(string $newVenueZip) {
-	// verify the venue zip is secure
-	$newVenueZip = trim($newVenueZip);
+		// verify the venue zip is secure
+		$newVenueZip = trim($newVenueZip);
 		$newVenueZip = filter_var($newVenueZip, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 		if(empty($newVenueZip) === true) {
-		throw(new \InvalidArgumentException("venue zip is empty or insecure"));
+			throw(new \InvalidArgumentException("venue zip is empty or insecure"));
 		}
 
 		// verify the venue zip content will fit into the database
 		if(strlen($newVenueZip) > 10) {
-		throw(new \RangeException("venue zip content too large"));
-	}
+			throw(new \RangeException("venue zip content too large"));
+		}
 
 		// store the venue zip content
 		$this->venueZip = $newVenueZip;
 	}
 
-		/**
-		 * formats the state variables for JSON serialization
-		 *
-		 * @return array resulting state variables to serialize
-		 **/
+	/**
+	 * inserts this venue into mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function insert(\PDO $pdo) {
+		// enforce the venueId is null (i.e., don't insert a venue that already exists)
+		if($this->venueId !== null) {
+			throw(new \PDOException("not a new venue"));
+		}
+
+		// create query template
+		$query = "INSERT INTO venue(venueProfileId, venueName, venueStreet1, venueStreet2, venueCity, venueState, venueZip) VALUES(:venueProfileId, :venueName, :venueStreet1, :venueStreet2, :venueCity, :venueState, :venueZip)";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders in the template
+		$parameters = ["venueProfileId" => $this->venueProfileId, "venueName" => $this->venueName, "venueStreet1" => $this->venueStreet1, "venueStreet2" => $this->venueStreet2, "venueCity" => $this->venueCity, "venueState" => $this->venueState, "venueZip" => $this->venueZip];
+		$statement->execute($parameters);
+
+		// update the null venueId with what mySQL just gave us
+		$this->venueId = intval($pdo->lastInsertId());
+	}
+
+	/**
+	 * updates this venue in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update(\PDO $pdo) {
+		// enforce the venueId is not null (i.e., don't update a venue that hasn't been inserted)
+		if($this->VenueId === null) {
+			throw(new \PDOException("unable to update a venue that does not exist"));
+		}
+
+		// create query template
+		$query = "UPDATE venue SET venueProfileId = :venueProfileId, venueName = :venueName, venueStreet1 = :venueStreet1, venueStreet2 = :venueStreet2, venueCity = :venueCity, venueState = :venueState, venueZip = venueZip WHERE venueId = :venueId";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders in the template\
+		$parameters = ["venueProfileId" => $this->venueProfileId, "venueName" => $this->venueName, "venueStreet1" => $this->venueStreet1, "venueStreet2" => $this->venueStreet2, "venueCity" => $this->venueCity, "venueState" => $this->venueState, "venueZip" => $this->venueZip];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * deletes this venue from mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function delete(\PDO $pdo) {
+		// enforce the venueId is not null (i.e., don't delete a venue that hasn't been inserted)
+		if($this->venueId === null) {
+			throw(new \PDOException("unable to delete a venue that does not exist"));
+		}
+
+		// create query template
+		$query = "DELETE FROM venue WHERE venueId = :venueId";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holder in the template
+		$parameters = ["venueId" => $this->venueId];
+		$statement->execute($parameters);
+	}
+
+	/**
+	 * gets the venue by venueId
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $venueId venue id to search for
+	 * @return Venue|null Venue found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getVenueByVenueId(\PDO $pdo, int $venueId) {
+		// sanitize the venueId before searching
+		if($venueId <= 0) {
+			throw(new \PDOException("venue id is not positive"));
+		}
+
+		// create query template
+		$query = "SELECT venueId, venueProfileId, venueStreet1, venueStreet2, VvenueCity, VenueState, venueZip FROM venue WHERE venueId = :venueId";
+		$statement = $pdo->prepare($query);
+
+		// bind the venue id to the place holder in the template
+		$parameters = ["venueId" => $venueId];
+		$statement->execute($parameters);
+
+		// grab the venue from mySQL
+		try {
+			$venue = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$venue = new Venue($row["venueId"], $row["venueProfileId"], $row["venueName"], $row["venueStreet1"], $row["venueStreet2"], $row["venueCity"], $row["venueState"], $row["venueZip"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return($venue);
+	}
+
+	/**
+	 * formats the state variables for JSON serialization
+	 *
+	 * @return array resulting state variables to serialize
+	 **/
 	public function jsonSerialize() {
-	$fields = get_object_vars($this);
-	return($fields);
-}
+		$fields = get_object_vars($this);
+		return ($fields);
+	}
 
 }

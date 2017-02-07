423e137b2466ebcf49905a9cf9a787b8e7ddc5ea
Joseph Ramirez <jramirez98@cnm.edu>
2017-02-07 11:20:29 -0700

    Created test file for Post class https://app.asana.com/0/256424297438824/258096775823627

---
+++ php/classes/test/PostTest.php
@@ -0,0 +1,20 @@
+<?php
+namespace Edu\Cnm\Jramirez98\GigHub\Test;
+
+use Edu\Cnm\Dmcdonald21\DataDesign\{Profile, Post };
+
+// grab the project test parameters
+require_once("DataDesignTest.php");
+
+// grab the class under scrutiny
+require_once(dirname(__DIR__) . "/classes/autoload.php");
+
+/**
+ * Full PHPUnit test for the Post class
+ *
+ * This is a complete PHPUnit test of the Post class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Post
+ * @author Joseph Ramirez <jramirez98@cnm.edu>
+ **/

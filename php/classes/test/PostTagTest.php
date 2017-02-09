78a2db1fc78fbd791c3e0678dca9701b2a6c80d8
Brandon Steider <bsteider@cnm.edu>
2017-02-09 10:09:46 -0700

    working on the maf still https://app.asana.com/0/256424297438824/258096775823636

--- php/classes/test/PostTagTest.php
+++ php/classes/test/PostTagTest.php
@@ -22,11 +22,61 @@ require_once(dirname(__DIR__) . "/classes/autoload.php");
 class PostTagTest extends GigHubTest {
 	/**
 	 * content of the PostTag
+	 * @var string $VALID_POSTTAG
+	 **/
+	protected $VALID_POSTTAG = "PHPUnit test passing";
+	/**
+	 * content of the updated PostTagId
 	 * @var string $VALID_POSTTAGID
 	 **/
-	protected $VALID_POSTTAGID = "PHPUnit test passing";
+	protected $VALID_POSTTAGID = "PHPUnit test still passing";
 	/**
-	 * content of the updated PostTag
-	 * @string $VALID_POSTTAGID
-	 */
+	 * id of the post tag tag id
+	 * @var string $VALID_POSTTAGTAGID
+	 **/
+	protected $VALID_POSTTAGTAGID = null;
+	/**
+	 * id of the post tag post id
+	 * @var string $VALID_POSTTAGPOSTID
+	 **/
+	protected $VALID_POSTTAGPOSTID = null;
+	/**
+	 * create dependent objects before running each test
+	 **/
+	public final function setUp() {
+		// run the default setUp() method first
+		parent::setUp();
+
+		// create and insert a Profile to own the test PostTag
+		$this->profile = new Profile(null, "@phpunit", "test@phpunit.de", "121255512120");
+		$this->profile-insert($this->getPDO());
+
+		// calculate the data (just use the time the unit test was setup)
+		$this->VALID_PROFILEDATE = new \DateTime();
+			/***********************
+			 * do i need the date because the post tag and profile tag classes dont use date?
+			 *************************/
+	}
+
+	/**
+	 * test inserting a valid postTag and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidPostTag() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("postTag");
+
+		// create a new post tag and insert into mySQL
+		$postTag = new PostTag(null, $this->profile->getProfileId(), $this->VALID_POSTTAGCONTENT, $this->VALID_POSTTAGDATE)
+			/*****************************
+			 * FER REAL THO DO I NEED THIS DATE STUFF
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *
+			 *****************************/
+	}
 }

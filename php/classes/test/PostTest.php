8c7210fbe51ed79ee326dd9d4a9abcbc19dea485
Joseph Ramirez <jramirez98@cnm.edu>
2017-02-09 12:01:07 -0700

    Created test create post than delete (line84) https://app.asana.com/0/256424297438824/258096775823627

--- php/classes/test/PostTest.php
+++ php/classes/test/PostTest.php
@@ -62,7 +62,7 @@ public function TestUpdateValidPost() {
 	$post->update($this->getPDO());
 
 	// grab the data from mySQL and enforce the fields match our expectations
-	$pdoPost = Post::getPostbyPostId ($this->getPDO(), $post->getPostId());
+	$pdoPost = Post::getPostby ($this->getPDO(), $post->get());
 	$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("post"));
 	$this->assertEquals($pdoPost-getpostProfileId(), $this->profile->getPostProfileId());
 	$this->assertEquals($pdoPost->getPostContent(), $this->VALID_POSTCONTENT2);
@@ -79,5 +79,26 @@ public function testUpdateInvalidPost() {
 	// create a post, try to update it without actually updating it and watch it fail
 	$post = new Post(null, $this->profile-getPostProfileId(), $this->VALID_POSTCONTENT, $this->VALID_POSTCREATEDDATE, $this->VALID_POSTEVENTDATE);
 	$post->update($this->getPDO());
+}
+
+/**
+ * test creating a post and then deleting it
+ */
+public function testDeleteValidPost() {
+	 // count the number of rows and save it for later
+	$numRows = $this->getConnection()->getRowCount("post");
+
+	// create a new post and insert into mySQL
+	$post = new Post(null, $this->profile->getProfileId, $this-> $this->getConnection()->getRowCount("post"));
+	$post->insert($this->getPDO());
+
+	// delete the Post from mySQL
+	$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("post"));
+	$post->delete($this->getPDO());
+
+	// grab the data from mySQL and enforce the Post does not exist
+	$pdoPost = Post::getPostProfilebyPostProfileId($this->getPDO(), $post->getPostProfileId());
+	$this->assertNUll($pdoPost);
+	$this->assertEquals($numRows, $this->getConnection()->getRowCount("post"));
 }
 

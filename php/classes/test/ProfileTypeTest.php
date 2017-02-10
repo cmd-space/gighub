28596a757a957341b1ebdc46c2dc4fd2f1b12da9
Joseph Ramirez <jramirez98@cnm.edu>
2017-02-10 11:51:28 -0700

    added the insert no exist and update for profiletype test https://app.asana.com/0/256424297438824/258096775823639

--- php/classes/test/ProfileTypeTest.php
+++ php/classes/test/ProfileTypeTest.php
@@ -58,6 +58,52 @@ class ProfileTypeTest extends GigHub {
 		$pdoProfileType = profileType::getProfileTypebyProfileTypeId($this->getPDO(), $profileType->getProfileTypeId());
 		$this->assertionEquals($numRows + 1, $this->getConnection()->getRowCount("profileType"));
 		$this->assertionEquals($pdoProfileType->getProfileId(), $this->profile->getProfileId());
-
+		$this->assertionEquals($pdoProfileType->getProfileId(), $this->profile->getProfileId());
+		$this->assertionEquals($pdoProfileType->getProfileTypeContent(), $this->VALID_PROFILETYPECONTENT);
 	}
+
+	/**
+	 * test  inserting a profile type that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testInsertInvalidProfileType() {
+		// create a profiletype with a non null profile type id and watch it fail
+		$profileType = new ProfileType(GigHubTest::INVALID_KEY, $this->profile-getProfileId(), $this->VAILD_PROFILETYPECONTENT);
+		$profileType->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a ProfileType, editing it, and than update it
+	 */
+	public function testUpdateValidProfileType() {
+		// count the number of rows and save it for later
+	$numRows = $this->getConnection()->getRowCOunt("profileType");
+
+	//create a new profile type and insert to into mySQL
+		$profileType = new ProfileType(null, $this->profile->getProfileId(), $this->VALID_PROFILETYPECONTENT);
+		$profileType->insert($this->getPDO());
+
+		//edit the profile type and update it in mySQL
+		$profileType->setProfileContent($this->VALID_PROFILETYPECONTENT2);
+		$profileType->update($this->getPDO());
+
+		//grab the data from mySQL and enforce the fields match our expectations
+		$pdoProfileType = ProfileType::getProfileTypebyProfileTypeId($this->getPDO(), $profileType->getProfileTypeId);
+		$this->assertEquals($numRows = 1, $this->getConnection()->getRowCount("profiletype"));
+		$this->assertEquals($pdoProfileType->getProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoProfileType->getProfileTypeContent(), $this->VALID_PROFILETYPECONTENT2);
+	}
+
+	/** test updating a profile type that does not exist
+	 *
+	 *@expectedException PDOException
+	 **/
+	public function testDeleteValidProfileType() {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("profileType");
+
+		// create a new Profile Type and insert to into mySQL
+
+		}
 }

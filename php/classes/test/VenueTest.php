f865740716c3e965680126c304aaa3a7d1d53da9
Dante Conley <danteconley@icloud.com>
2017-02-10 11:19:42 -0700

    test grabbing a venue by zip, test grabbing a venue by zip that does not exist. https://app.asana.com/0/256424297438824/258096775823632

--- php/classes/test/VenueTest.php
+++ php/classes/test/VenueTest.php
@@ -265,7 +265,6 @@ class VenueTest extends GigHubTest {
 		$venue = new Venue(null, $this->profile->getVenueProfileId(), $this->VALID_VENUENAME, $this->VALID_VENUESTREET1, $this->VALID_VENUESTREET2, $this->VALID_VENUECITY, $this->VALID_VENUESTATE, $this->VALID_VENUEZIP);
 		$venue->insert($this->getPDO());
 
-
 		// grab the data from mySQL and enforce the fields match our expectations
 		$results = Venue::getVenueByVenueCity($this->getPDO(), $venue->getVenueCity());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("venue"));
@@ -274,7 +273,6 @@ class VenueTest extends GigHubTest {
 
 		// grab the result from the array and validate it
 		$pdoTweet = $results[0];
-		$this->assertEquals($pdoTweet->getProfileId(), $this->profile->getProfileId());
 		$this->assertEquals($pdoVenue->getVenueProfileId(), $this->profile->getVenueProfileId());
 		$this->assertEquals($pdoVenue->getVenueName(), $this->VALID_VENUENAME);
 		$this->assertEquals($pdoVenue->getVenueStreet1(), $this->VALID_VENUESTREET1);
@@ -290,5 +288,42 @@ class VenueTest extends GigHubTest {
 	public function testGetInvalidVenueByVenueCity() {
 		// grab a venur by searching for a city that does not exist
 		$venue = Venue::getVenueByVenueCity($this->getPDO(), "imaginary venue city, dummy");
+		$this->assertCount(0, $venue);
+	}
+
+	/**
+	 * test grabbing a Venue by Venue Zip
+	 **/
+	public function testGetValidVenueByVenueZip() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("venue");
+
+		// create a new Venue and insert to into mySQL
+		$venue = new Venue(null, $this->profile->getVenueProfileId(), $this->VALID_VENUENAME, $this->VALID_VENUESTREET1, $this->VALID_VENUESTREET2, $this->VALID_VENUECITY, $this->VALID_VENUESTATE, $this->VALID_VENUEZIP);
+		$venue->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$results = Venue::getVenueByVenueZip($this->getPDO(), $venue->getVenueZip());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("venue"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\Dconley6\\GigHub\\Venue", $results);
+
+		// grab the result from the array and validate it
+		$pdoVenue = $results[0];
+		$this->assertEquals($pdoVenue->getVenueProfileId(), $this->profile->getVenueProfileId());
+		$this->assertEquals($pdoVenue->getVenueName(), $this->VALID_VENUENAME);
+		$this->assertEquals($pdoVenue->getVenueStreet1(), $this->VALID_VENUESTREET1);
+		$this->assertEquals($pdoVenue->getVenueStreet2(), $this->VALID_VENUESTREET2);
+		$this->assertEquals($pdoVenue->getVenueCity(), $this->VALID_VENUECITY);
+		$this->assertEquals($pdoVenue->getVenueState(), $this->VALID_VENUESTATE);
+		$this->assertEquals($pdoVenue->getVenueZip(), $this->VALID_VENUEZIP);
+	}
+
+	/**
+	 * test grabbing a Venue by Zip that does not exist
+	 **/
+	public function testGetInvalidVenueByVenueZip() {
+		// grab a venue by searching for zip that does not exist
+		$venue = Venue::getVenueByVenueZip($this->getPDO(), "imaginary venue zip, dummy");
 		$this->assertCount(0, $venue);
 	}

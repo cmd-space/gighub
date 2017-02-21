a7651648c8ddf61aa15c2f12a052ba56001c72bb
Mason Crane <masonecrane@gmail.com>
2017-02-20 20:20:48 -0700

    cleaned up some code, and adjusted doc block @return to properly represent what should be returned

--- php/classes/ProfileType.php
+++ php/classes/ProfileType.php
@@ -164,32 +164,33 @@ class ProfileType implements \JsonSerializable {
 	 *
 	 * @param \PDO $pdo PDO connection object
 	 * @param int $profileTypeId profileType found or null if not found
-	 * @return ProfileTypeId|null ProfileType found or null if not found
+	 * @return ProfileType|null ProfileType found or null if not found
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError when variable are not the correct data type
 	 **/
 	public static function getProfileTypeByProfileTypeId(\PDO $pdo, int $profileTypeId) {
 		// sanitize the profileTypeId before searching
-	if($profileTypeId <= 0) {
-		throw(new \PDOException("Profile Type id is not positive"));
-}
+		if($profileTypeId <= 0) {
+			throw(new \PDOException("Profile Type id is not positive"));
+		}
 
-// create query template
+		// create query template
 		$query = "SELECT profileTypeId, profileTypeName FROM profileType WHERE profileTypeId = :profileTypeId";
-	$statement = $pdo = $pdo->prepare($query);
+		$statement = $pdo->prepare($query);
 
-	// bind the profile type id to the place holder in the template
-		$parameters =  ["profileTypeId" => $profileTypeId];
+		// bind the profile type id to the place holder in the template
+		$parameters = ["profileTypeId" => $profileTypeId];
 		$statement->execute($parameters);
 
 		// grab the profile type from mySQL
 		try {
-				$profileType = null;
-				$statement->setFetchMode(\PDO::FETCH_ASSOC);
-				$row = $statement->fetch(); {
+			$profileType = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
 					$profileType = new ProfileType($row["profileTypeId"], $row["profileTypeName"]);
 			}
-		}catch(\Exception $exception) {
+		} catch(\Exception $exception) {
 			// if the row couldn't be converted, rethrow it
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}

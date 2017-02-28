f770875b00cf3d724a2339b9ed1c200ca0b16430
Mason Crane <masonecrane@gmail.com>
2017-02-28 11:16:43 -0700

    added namespace, added foreach to set session data, added new profile PDO
    Also added TypeError catch and altered Exception catch to be more specific
    
    https://app.asana.com/0/256424297438824/276564115247375

--- public_html/api/oauth/index.php
+++ public_html/api/oauth/index.php
@@ -1,13 +1,12 @@
 <?php
+namespace Edu\Cnm\GigHub\Profile;
 
 require_once( dirname( __DIR__, 3 ) . "/vendor/autoload.php" );
 require_once( dirname( __DIR__, 3 ) . "/php/classes/autoload.php" );
 require_once( "/etc/apache2/capstone-mysql/encrypted-config.php" );
 
 try {
-
-
-//grab the MySQL connection
+	//grab the MySQL connection
 	$pdo    = connectToEncryptedMySQL( "/etc/apache2/capstone-mysql/gighub.ini" );
 	$config = readConfig( "/etc/apache2/capstone-mysql/gighub.ini" );
 	$oauth  = json_decode( $config["oauth"] );
@@ -20,17 +19,46 @@ try {
 	if ( ! isset( $_GET['code'] ) ) {
 		$auth_url = $client->getAuthenticationUrl( AUTHORIZATION_ENDPOINT, REDIRECT_URI );
 		header( 'Location: ' . $auth_url );
-		exit;
+		die( 'Redirect' );
 	} else {
 		$params   = array( 'code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI );
 		$response = $client->getAccessToken( TOKEN_ENDPOINT, 'authorization_code', $params );
 		parse_str( $response['result'], $info );
 		$client->setAccessToken( $info['access_token'] );
 		$response = $client->fetch( 'https://graph.facebook.com/me' );
+//	    TODO: reinstate this foreach after var_dump verifies actual return values
+//      foreach ( $response['result'] as $result ) {
+//			if ( $result['primary'] === true ) {
+//				$profileOAuthToken = $result[''];
+//				break;
+//			}
+//		}
 		var_dump( $response, $response['result'] );
 	}
+
+// TODO: uncomment below code after verifying var_dump data
+	// get profile by email to see if it exists, if it does not then create a new one
+//	$profile = Profile::getProfileByProfileOAuthToken($pdo, $profileOAuthToken);
+//	if(empty($profile) === true) {
+//		// create a new profile
+//		$profile = new Profile(null, "Please update your profile content!", "Please update your location!");
+//		$profile->insert($pdo);
+//		$reply->message = "Welcome to GigHub! Party on!";
+//	} else {
+//		$reply->message = "So glad to see you back!";
+//	}
+//	//grab profile from database and put into a session
+//	$profile = Profile::getProfileByProfileOAuthToken($pdo, $profileOAuthToken);
+//	$_SESSION["profile"] = $profile;
+//	header("Location: ../../../");
+
 } catch(\Exception $exception) {
-	throw(new);
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
+	$reply->trace = $exception->getTraceAsString();
+} catch(\TypeError $typeError) {
+	$reply->status = $typeError->getCode();
+	$reply->message = $typeError->getMessage();
 }
 //How can I add a new Grant Type ?
 //================================
@@ -61,7 +89,7 @@ class MyCustomGrantType implements IGrantType {
 		if ( ! isset( $parameters['first_mandatory_parameter'] ) ) {
 			throw new \Exception( 'The \'first_mandatory_parameter\' parameter must be defined for the Password grant type' );
 		} elseif ( ! isset( $parameters['second_mandatory_parameter'] ) ) {
-			throw new \Exception( 'The \'seconde_mandatory_parameter\' parameter must be defined for the Password grant type' );
+			throw new \Exception( 'The \'second_mandatory_parameter\' parameter must be defined for the Password grant type' );
 		}
 	}
 }

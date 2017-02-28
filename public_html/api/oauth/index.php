02298fda5de0ebce3f5cbc3622a94eede552e5ec
Mason Crane <masonecrane@gmail.com>
2017-02-28 13:20:43 -0700

    removed class
    https://app.asana.com/0/256424297438824/276564115247375

--- public_html/api/oauth/index.php
+++ public_html/api/oauth/index.php
@@ -73,43 +73,43 @@ try {
 	$reply->status = $typeError->getCode();
 	$reply->message = $typeError->getMessage();
 }
-//How can I add a new Grant Type ?
-//================================
-//Simply write a new class in the namespace OAuth2\GrantType. You can place the class file under GrantType.
-//Here is an example :
-
-namespace OAuth2\GrantType;
-
-/**
- * MyCustomGrantType Grant Type
- */
-class MyCustomGrantType implements IGrantType {
-	/**
-	 * Defines the Grant Type
-	 *
-	 * @var string  Defaults to 'my_custom_grant_type'.
-	 */
-	$GRANT_TYPE = 'my_custom_grant_type';
-
-	/**
-	 * Adds a specific Handling of the parameters
-	 *
-	 * @return array of Specific parameters to be sent.
-	 *
-	 * @param  mixed $parameters the parameters array (passed by reference)
-	 *
-	 * @throws \Exception if first mandatory parameter is set
-	 *
-	 * @throws \Exception if second mandatory parameter is set
-	 */
-	public function validateParameters( &$parameters ) {
-		if ( ! isset( $parameters['first_mandatory_parameter'] ) ) {
-			throw new \Exception( 'The \'first_mandatory_parameter\' parameter must be defined for the Password grant type' );
-		} elseif ( ! isset( $parameters['second_mandatory_parameter'] ) ) {
-			throw new \Exception( 'The \'second_mandatory_parameter\' parameter must be defined for the Password grant type' );
-		}
-	}
-}
+////How can I add a new Grant Type ?
+////================================
+////Simply write a new class in the namespace OAuth2\GrantType. You can place the class file under GrantType.
+////Here is an example :
+//
+//namespace OAuth2\GrantType;
+//
+///**
+// * MyCustomGrantType Grant Type
+// */
+//class MyCustomGrantType implements IGrantType {
+//	/**
+//	 * Defines the Grant Type
+//	 *
+//	 * @var string  Defaults to 'my_custom_grant_type'.
+//	 */
+//	const GRANT_TYPE = 'my_custom_grant_type';
+//
+//	/**
+//	 * Adds a specific Handling of the parameters
+//	 *
+//	 * @return array of Specific parameters to be sent.
+//	 *
+//	 * @param  mixed $parameters the parameters array (passed by reference)
+//	 *
+//	 * @throws \Exception if first mandatory parameter is set
+//	 *
+//	 * @throws \Exception if second mandatory parameter is set
+//	 */
+//	public function validateParameters( &$parameters ) {
+//		if ( ! isset( $parameters['first_mandatory_parameter'] ) ) {
+//			throw new \Exception( 'The \'first_mandatory_parameter\' parameter must be defined for the Password grant type' );
+//		} elseif ( ! isset( $parameters['second_mandatory_parameter'] ) ) {
+//			throw new \Exception( 'The \'second_mandatory_parameter\' parameter must be defined for the Password grant type' );
+//		}
+//	}
+//}
 
 //call the OAuth client getAccessToken with the grantType you defined in the GRANT_TYPE constant, As following :
 $response = $client->getAccessToken( TOKEN_ENDPOINT, 'my_custom_grant_type', $params );

e3b819ad3fcb187699082a53116fa2f01420ff92
Mason Crane <masonecrane@gmail.com>
2017-02-28 13:14:53 -0700

    added xsrf.php, started session, and prepared an empty $reply for use in exceptions
    https://app.asana.com/0/256424297438824/276564115247375

--- public_html/api/oauth/index.php
+++ public_html/api/oauth/index.php
@@ -1,9 +1,22 @@
 <?php
+
 namespace Edu\Cnm\GigHub\Profile;
 
-require_once( dirname( __DIR__, 3 ) . "/vendor/autoload.php" );
 require_once( dirname( __DIR__, 3 ) . "/php/classes/autoload.php" );
+require_once (dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once( dirname( __DIR__, 3 ) . "/vendor/autoload.php" );
 require_once( "/etc/apache2/capstone-mysql/encrypted-config.php" );
+
+use Edu\Cnm\GigHub\Profile;
+
+// verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+// prepare an empty reply
+$reply = new \stdClass();
+$reply->status = 200;
+$reply->data = null;
 
 try {
 	//grab the MySQL connection
@@ -11,9 +24,9 @@ try {
 	$config = readConfig( "/etc/apache2/capstone-mysql/gighub.ini" );
 	$oauth  = json_decode( $config["oauth"] );
 
-	$REDIRECT_URI           = 'https://bootcamp-coders.cnm.edu/~jramirez98/gighub/public_html/api/oauth/';
-	$AUTHORIZATION_ENDPOINT = 'https://www.facebook.com/v2.8/dialog/oauth';
-	$TOKEN_ENDPOINT         = 'https://graph.facebook.com/v2.8/oauth/access_token';
+	const REDIRECT_URI           = 'https://bootcamp-coders.cnm.edu/~jramirez98/gighub/public_html/api/oauth/';
+	const AUTHORIZATION_ENDPOINT = 'https://www.facebook.com/v2.8/dialog/oauth';
+	const TOKEN_ENDPOINT         = 'https://graph.facebook.com/v2.8/oauth/access_token';
 
 	$client = new \OAuth2\Client( $oauth->facebook->clientId, $oauth->facebook->clientKey );
 	if ( ! isset( $_GET['code'] ) ) {
@@ -27,30 +40,30 @@ try {
 		$client->setAccessToken( $info['access_token'] );
 		$response = $client->fetch( 'https://graph.facebook.com/me' );
 //	    TODO: reinstate this foreach after var_dump verifies actual return values
-//      foreach ( $response['result'] as $result ) {
-//			if ( $result['primary'] === true ) {
-//				$profileOAuthToken = $result[''];
-//				break;
-//			}
-//		}
+      foreach ( $response['result'] as $result ) {
+			if ( $result['primary'] === true ) {
+				$profileOAuthToken = $result[''];
+				break;
+			}
+		}
 		var_dump( $response, $response['result'] );
 	}
 
 // TODO: uncomment below code after verifying var_dump data
 	// get profile by email to see if it exists, if it does not then create a new one
-//	$profile = Profile::getProfileByProfileOAuthToken($pdo, $profileOAuthToken);
-//	if(empty($profile) === true) {
-//		// create a new profile
-//		$profile = new Profile(null, "Please update your profile content!", "Please update your location!");
-//		$profile->insert($pdo);
-//		$reply->message = "Welcome to GigHub! Party on!";
-//	} else {
-//		$reply->message = "So glad to see you back!";
-//	}
-//	//grab profile from database and put into a session
-//	$profile = Profile::getProfileByProfileOAuthToken($pdo, $profileOAuthToken);
-//	$_SESSION["profile"] = $profile;
-//	header("Location: ../../../");
+	$profile = Profile::getProfileByProfileOAuthToken($pdo, $profileOAuthToken);
+	if(empty($profile) === true) {
+		// create a new profile
+		$profile = new Profile(null, "Please update your profile content!", "Please update your location!");
+		$profile->insert($pdo);
+		$reply->message = "Welcome to GigHub! Party on!";
+	} else {
+		$reply->message = "So glad to see you back!";
+	}
+	//grab profile from database and put into a session
+	$profile = Profile::getProfileByProfileOAuthToken($pdo, $profileOAuthToken);
+	$_SESSION["profile"] = $profile;
+	header("Location: ../../../");
 
 } catch(\Exception $exception) {
 	$reply->status = $exception->getCode();
@@ -84,6 +97,10 @@ class MyCustomGrantType implements IGrantType {
 	 * @return array of Specific parameters to be sent.
 	 *
 	 * @param  mixed $parameters the parameters array (passed by reference)
+	 *
+	 * @throws \Exception if first mandatory parameter is set
+	 *
+	 * @throws \Exception if second mandatory parameter is set
 	 */
 	public function validateParameters( &$parameters ) {
 		if ( ! isset( $parameters['first_mandatory_parameter'] ) ) {

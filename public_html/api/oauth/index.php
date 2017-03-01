3e1b5dab3245011de8f2592e149b2c577079b441
Mason Crane <masonecrane@gmail.com>
2017-03-01 13:11:40 -0700

    added require_once dependencies
    https://app.asana.com/0/256424297438824/276564115247375

--- public_html/api/oauth/index.php
+++ public_html/api/oauth/index.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once (dirname(__DIR__, 3) . "/php/classes/autoload.php");
+require_once (dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
 //grab the MySQL connection
 $pdo    = connectToEncryptedMySQL( "/etc/apache2/capstone-mysql/gighub.ini" );
 $config = readConfig( "/etc/apache2/capstone-mysql/gighub.ini" );
--- public_html/facebook-login/index.php
+++ public_html/facebook-login/index.php
@@ -1,7 +1,12 @@
 <?php
 
+require_once (dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once (dirname(__DIR__, 3) . "/php/classes/autoload.php");
+require_once (dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
 //grab the MySQL connection
-$pdo    = connectToEncryptedMySQL( "/etc/apache2/capstone-mysql/gighub.ini" );
+//$pdo    = connectToEncryptedMySQL( "/etc/apache2/capstone-mysql/gighub.ini" );
 $config = readConfig( "/etc/apache2/capstone-mysql/gighub.ini" );
 $oauth  = json_decode( $config["oauth"] );
 

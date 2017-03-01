62121f74bb64bef805a8ef1c1e9a9dd590e3b9a6
Joseph Ramirez <jramirez98@cnm.edu>
2017-03-01 09:26:47 -0700

    made changes on line 33 https://app.asana.com/0/252717071352384/276564115247375

--- public_html/api/oauth/index.php
+++ public_html/api/oauth/index.php
@@ -30,12 +30,12 @@ try {
 
 	$client = new \OAuth2\Client( $oauth->facebook->clientId, $oauth->facebook->clientKey );
 	if ( ! isset( $_GET['code'] ) ) {
-		$auth_url = $client->getAuthenticationUrl( AUTHORIZATION_ENDPOINT, REDIRECT_URI );
+		$auth_url = $client->getAuthenticationUrl( $AUTHORIZATION_ENDPOINT, $REDIRECT_URI );
 		header( 'Location: ' . $auth_url );
 		die( 'Redirect' );
 	} else {
-		$params   = array( 'code' => $_GET['code'], 'redirect_uri' => REDIRECT_URI );
-		$response = $client->getAccessToken( TOKEN_ENDPOINT, 'authorization_code', $params );
+		$params   = array( 'code' => $_GET['code'], 'redirect_uri' => $REDIRECT_URI );
+		$response = $client->getAccessToken( $TOKEN_ENDPOINT, 'authorization_code', $params );
 		parse_str( $response['result'], $info );
 		$client->setAccessToken( $info['access_token'] );
 		$response = $client->fetch( 'https://graph.facebook.com/me' );
@@ -73,43 +73,3 @@ try {
 	$reply->status = $typeError->getCode();
 	$reply->message = $typeError->getMessage();
 }
-////How can I add a new Grant Type ?
-////================================
-////Simply write a new class in the namespace OAuth2\GrantType. You can place the class file under GrantType.
-////Here is an example :
-//
-//namespace OAuth2\GrantType;
-//
-///**
-// * MyCustomGrantType Grant Type
-// */
-//class MyCustomGrantType implements IGrantType {
-//	/**
-//	 * Defines the Grant Type
-//	 *
-//	 * @var string  Defaults to 'my_custom_grant_type'.
-//	 */
-//	const GRANT_TYPE = 'my_custom_grant_type';
-//
-//	/**
-//	 * Adds a specific Handling of the parameters
-//	 *
-//	 * @return array of Specific parameters to be sent.
-//	 *
-//	 * @param  mixed $parameters the parameters array (passed by reference)
-//	 *
-//	 * @throws \Exception if first mandatory parameter is set
-//	 *
-//	 * @throws \Exception if second mandatory parameter is set
-//	 */
-//	public function validateParameters( &$parameters ) {
-//		if ( ! isset( $parameters['first_mandatory_parameter'] ) ) {
-//			throw new \Exception( 'The \'first_mandatory_parameter\' parameter must be defined for the Password grant type' );
-//		} elseif ( ! isset( $parameters['second_mandatory_parameter'] ) ) {
-//			throw new \Exception( 'The \'second_mandatory_parameter\' parameter must be defined for the Password grant type' );
-//		}
-//	}
-//}
-
-//call the OAuth client getAccessToken with the grantType you defined in the GRANT_TYPE constant, As following :
-$response = $client->getAccessToken( TOKEN_ENDPOINT, 'my_custom_grant_type', $params );

dada3bb71a924495e66ee233c2208eae77bdab5e
Brandon Steider <bsteider@cnm.edu>
2017-03-04 13:13:27 -0700

    changed session

--- public_html/api/tag/index.php
+++ public_html/api/tag/index.php
@@ -71,7 +71,7 @@ try {
 
 		// Make sure that only one can edit one's own profile
 		$profile = Profile::getProfileByProfileOAuthToken($pdo, $oAuthToken);
-		if(empty($_SESSION["profileOAuthToken"]) === true || $_SESSION["profileOAuthToken"]->getProfileOAuthToken() !== $profile->getProfileOAuthToken()) {
+		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileOAuthToken() !== $profile->getProfileOAuthToken()) {
 			throw(new \InvalidArgumentException("You do not have permission to edit this profile... Login, why don't you? It's a quick little task. Just do it.", 403));
 		}
 		//make sure tag content is available (required field)

a83ce964e26b298b6aaf6870ea674a54c6cbd5ce
Mason Crane <masonecrane@gmail.com>
2017-03-05 12:18:34 -0700

    added Profile class reference in use
    https://app.asana.com/0/256424297438824/276564115247382

--- public_html/api/profileType/index.php
+++ public_html/api/profileType/index.php
@@ -5,7 +5,7 @@ require_once (dirname(__DIR__, 3) . "/php/classes/autoload.php");
 require_once (dirname(__DIR__, 3) . "/php/lib/xsrf.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Edu\Cnm\GigHub\ProfileType;
+use Edu\Cnm\GigHub\{Profile, ProfileType};
 
 /**
  * 1. start off by creating an ordinary FB account

6a2a05de4739afbb51f15c1abff86add83f7fd79
Brandon Steider <bsteider@cnm.edu>
2017-03-08 09:29:46 -0700

    just changing up links and such https://app.asana.com/0/256424297438824/285635862591172

--- epic/frontend-sandbox/public_html/index.php
+++ epic/frontend-sandbox/public_html/index.php
@@ -33,7 +33,6 @@
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Brand</a>
 				</div>
 
 				<!-- Collect the nav links, forms, and other content for toggling -->
@@ -41,13 +40,7 @@
 					<ul class="nav navbar-nav">
 						<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
 						<li><a href="#">Link</a></li>
-								<li><a href="#">Action</a></li>
-								<li><a href="#">Another action</a></li>
-								<li><a href="#">Something else here</a></li>
-								<li role="separator" class="divider"></li>
-								<li><a href="#">Separated link</a></li>
-								<li role="separator" class="divider"></li>
-								<li><a href="#">One more separated link</a></li>
+								<li><a href="#">Sign Out</a></li>
 							</ul>
 						</li>
 					</ul>
@@ -59,9 +52,6 @@
 					</form>
 					<ul class="nav navbar-nav navbar-right">
 						<li><a href="#">Link</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-							<ul class="dropdown-menu">
 								<li><a href="#">Action</a></li>
 								<li><a href="#">Another action</a></li>
 								<li><a href="#">Something else here</a></li>

7519702b45ffc0623f0b6dddfc1d48a606257114
Mason Crane <masonecrane@gmail.com>
2017-03-08 14:27:10 -0700

    added modal, button to launch modal and filled out all fields for profile form

--- epic/frontend-sandbox/profile.php
+++ epic/frontend-sandbox/profile.php
@@ -1,27 +1,56 @@
-<!doctype html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<!--		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-		<title>GigHub</title>
-	</head>
+<?php require_once("lib/head-utils.php"); ?>
 	<body>
-
+		<?php require_once("nav.php"); ?>
+		<!-- Button trigger modal -->
+		<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#editProfileModal">
+			Launch profile modal
+		</button>
+		<div class="modal fade" tabindex="-1" role="dialog" id="editProfileModal">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+						<h4 class="modal-title">Edit Profile</h4>
+					</div>
+					<div class="modal-body">
+						<form action="#">
+							<div class="form-group">
+								<label for="inputFile">Profile Image</label>
+								<input type="file" class="form-control" id="inputFile">
+								<p class="help-block">Choose your profile image</p>
+							</div>
+							<div class="form-group">
+								<label for="profileType">Profile Type</label>
+								<select name="profileType" class="form-control" id="profileType">
+									<option value="1">Fan</option>
+									<option value="2">Artist</option>
+									<option value="3">Venue</option>
+								</select>
+							</div>
+							<div class="form-group">
+								<label for="profileUserName">Profile User Name</label>
+								<input class="form-control" type="text" id="profileUserName" name="profileUserName" placeholder="current user name here...">
+							</div>
+							<div class="form-group">
+								<label for="profileBio">Profile Bio</label>
+								<textarea class="form-control" name="profileBio" id="profileBio" cols="20" rows="7" placeholder="This will become their current profile bio..."></textarea>
+							</div>
+							<div class="form-group">
+								<label for="profileLocation">Location</label>
+								<input class="form-control" type="text" id="profileLocation" name="profileLocation" placeholder="current user location here...">
+							</div>
+							<div class="form-group">
+								<label for="profileSoundCloudUser">SoundCloud User Name (optional)</label>
+								<input class="form-control" type="text" id="profileSoundCloudUser" name="profileSoundCloudUser" placeholder="current SoundCloud user name here...">
+							</div>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						<button type="button" class="btn btn-primary">Save changes</button>
+					</div>
+				</div><!-- /.modal-content -->
+			</div><!-- /.modal-dialog -->
+		</div><!-- /.modal -->
 	</body>
 </html>

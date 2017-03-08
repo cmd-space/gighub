75aba0b8a2ee6419f99e0c6888ecb509fb10bec0
Dante Conley <danteconley@icloud.com>
2017-03-08 15:06:24 -0700

    added splash.php https://app.asana.com/0/256424297438824/258096775823632

---
+++ epic/frontend-sandbox/splash.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require_once("lib/head-utils.php");?>
+
+<body>
+
+<div class="container">x
+	<img src="../images/GigHubLogoWhite.png"><br><br>
+</div>
+
+</body>
+
+</html>x

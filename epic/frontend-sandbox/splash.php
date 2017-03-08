4713791d64753fd1ef220e907bb8df04426bebc4
Dante Conley <danteconley@icloud.com>
2017-03-08 15:34:55 -0700

    trying to get body css tag complete https://app.asana.com/0/256424297438824/258096775823632

--- epic/frontend-sandbox/splash.php
+++ epic/frontend-sandbox/splash.php
@@ -5,9 +5,22 @@
 
 <body>
 
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=1878867439052171";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));</script>
+
+
 <div class="container">
-	<img src="../images/GigHubLogoWhite.png"><br><br>
+	<img src="../images/GigHubWhite.png">
 </div>
+	<div class="container">
+		<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
+	</div>
 
 </body>
 

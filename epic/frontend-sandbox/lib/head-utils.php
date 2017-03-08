0ed84fbd1e0a8c0cd51c3e4f6ab069034cf5186d
Mason Crane <masonecrane@gmail.com>
2017-03-08 16:41:41 -0700

    altered css and changed path to css

--- epic/frontend-sandbox/lib/head-utils.php
+++ epic/frontend-sandbox/lib/head-utils.php
@@ -19,6 +19,6 @@
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-		<link rel="stylesheet" href="../css/style.css">
+		<link rel="stylesheet" href="css/style.css">
 		<title>GigHub</title>
 	</head>

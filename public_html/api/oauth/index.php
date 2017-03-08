6920ae2a1c07c026001a67aea301f346b188a23a
Mason Crane <masonecrane@gmail.com>
2017-03-08 09:20:11 -0700

    added try block to request id, name and email

--- public_html/api/oauth/index.php
+++ public_html/api/oauth/index.php
@@ -85,6 +85,21 @@ if ( ! $accessToken->isLongLived() ) {
 	var_dump( $accessToken->getValue() );
 }
 
+try {
+	// Returns a `Facebook\FacebookResponse` object
+	$response = $fb->get('/me?fields=id,name,email', $accessToken);
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+	echo 'Graph returned an error: ' . $e->getMessage();
+	exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+	echo 'Facebook SDK returned an error: ' . $e->getMessage();
+	exit;
+}
+
+$user = $response->getGraphUser();
+
+echo 'id: ' . $user['id'];
+
 $_SESSION['fb_access_token'] = (string) $accessToken;
 //var_dump($_SESSION['fb_access_token']);
 if(empty($_SESSION["fb_access_token"]) === true) {

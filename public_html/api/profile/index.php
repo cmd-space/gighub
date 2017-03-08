7d50d1c7a39a01aeb94f2d6e1ad1d38fb5cc6131
Mason Crane <masonecrane@gmail.com>
2017-03-08 14:48:52 -0700

    renamed sanitized inputs per George's wishes

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -1,5 +1,5 @@
 <?php
-//TODO: rename variables to match class
+
 require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
 require_once(dirname(__DIR__, 3) . "/php/classes/autoload.php");
 require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
@@ -34,53 +34,55 @@ try {
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
-	$oAuthId = filter_input(INPUT_GET, "oAuthId", FILTER_VALIDATE_INT);
-	$type = filter_input(INPUT_GET, "type", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$location = filter_input(INPUT_GET, "location", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$oAuthToken = filter_input(INPUT_GET, "oAuthToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$soundCloudUser = filter_input(INPUT_GET, "soundCloudUser", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$userName = filter_input(INPUT_GET, "userName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$id                = filter_input( INPUT_GET, "id", FILTER_VALIDATE_INT );
+	$profileOAuthId           = filter_input( INPUT_GET, "profileOAuthId", FILTER_VALIDATE_INT );
+	$profileTypeId              = filter_input( INPUT_GET, "profileTypeId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
+	$profileBio               = filter_input( INPUT_GET, "profileBio", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
+	$profileImageCloudinaryId = filter_input( INPUT_GET, "profileImageCloudinaryId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
+	$profileLocation          = filter_input( INPUT_GET, "profileLocation", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
+	$profileOAuthToken        = filter_input( INPUT_GET, "profileOAuthToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
+	$profileSoundCloudUser    = filter_input( INPUT_GET, "profileSoundCloudUser", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
+	$profileUserName          = filter_input( INPUT_GET, "profileUserName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES );
 
 	//make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
-		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	if ( ( $method === "DELETE" || $method === "PUT" ) && ( empty( $id ) === true || $id < 0 ) ) {
+		throw( new InvalidArgumentException( "id cannot be empty or negative", 405 ) );
 	}
 
 	// handle GET request - if id is present, that profile is returned
-	if($method === "GET") {
+	if ( $method === "GET" ) {
 		//set XSRF cookie
 		setXsrfCookie();
 
 		//get a specific profile
-		if(empty($id) === false) {
-			$profile = Profile::getProfileByProfileId($pdo, $id);
-			if($profile !== null) {
+		if ( empty( $id ) === false ) {
+			$profile = Profile::getProfileByProfileId( $pdo, $id );
+			if ( $profile !== null ) {
 				$reply->data = $profile;
 			}
-		} else if(empty($oAuthId) === false) {
-			$profile = Profile::getProfileByProfileOAuthId($pdo, $oAuthId);
-			if($profile !== null) {
+		} else if ( empty( $profileOAuthId ) === false ) {
+			$profile = Profile::getProfileByProfileOAuthId( $pdo, $profileOAuthId );
+			if ( $profile !== null ) {
 				$reply->data = $profile;
 			}
-		} else if(empty($type) === false) {
-			$profiles = Profile::getProfileByProfileTypeId($pdo, $type);
-			if($profiles !== null) {
+		} else if ( empty( $profileTypeId ) === false ) {
+			$profiles = Profile::getProfileByProfileTypeId( $pdo, $profileTypeId );
+			if ( $profiles !== null ) {
 				$reply->data = $profiles;
 			}
-		} else if(empty($location) === false) {
-			$profiles = Profile::getProfileByLocationContent($pdo, $location);
-			if($profiles !== null) {
+		} else if ( empty( $profileLocation ) === false ) {
+			$profiles = Profile::getProfileByLocationContent( $pdo, $profileLocation );
+			if ( $profiles !== null ) {
 				$reply->data = $profiles;
 			}
-		} else if(empty($soundCloudUser) === false) {
-			$profiles = Profile::getProfileBySoundCloudUser($pdo, $soundCloudUser);
-			if($profiles !== null) {
+		} else if ( empty( $profileSoundCloudUser ) === false ) {
+			$profiles = Profile::getProfileBySoundCloudUser( $pdo, $profileSoundCloudUser );
+			if ( $profiles !== null ) {
 				$reply->data = $profiles;
 			}
-		} else if(empty($userName) === false) {
-			$profiles = Profile::getProfileByProfileUserName($pdo, $userName);
-			if($profiles !== null) {
+		} else if ( empty( $profileUserName ) === false ) {
+			$profiles = Profile::getProfileByProfileUserName( $pdo, $profileUserName );
+			if ( $profiles !== null ) {
 				$reply->data = $profiles;
 			}
 		}

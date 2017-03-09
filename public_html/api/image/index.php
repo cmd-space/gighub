f72bd58ed03b7569a703651aba8b06a208a3a0d9
Mason Crane <masonecrane@gmail.com>
2017-03-09 12:01:34 -0700

    added session logic, status code and requires

--- public_html/api/image/index.php
+++ public_html/api/image/index.php
@@ -1,5 +1,26 @@
 <?php
 
+require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/classes/autoload.php");
+require_once(dirname(__DIR__, 3) . "/php/lib/xsrf.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+/**
+ * api for the images
+ *
+ * @author Mason Crane <cmd-space.com>
+ **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
 try {
 	//grab the mySQL connection
 	$pdo = connectToEncryptedMySQL( "/etc/apache2/capstone-mysql/gighub.ini" );

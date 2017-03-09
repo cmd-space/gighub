44a7471760d2a2d2302bc0fad01caef2551a3ead
Brandon Steider <bsteider@cnm.edu>
2017-03-09 13:40:54 -0700

    added img to footer https://app.asana.com/0/256424297438824/285635862591173

--- epic/frontend-sandbox/footer.php
+++ epic/frontend-sandbox/footer.php
@@ -1,11 +1,11 @@
 <footer class="navbar-fixed-bottom">
-	<div class="container" id="footer">
+	<div class="container-fluid" id="footer">
 		<div class="row">
-			<div class="col-sm-6">
-				About Us
+			<div class="col-sm-6 text-left-sm">
+				<img src="//gighubhorizontal.png" />
 			</div>
-			<div class="col-sm-6 text-right-sm">
-				Contact Us
+			<div class="col-md-6 c text-right-sm ">
+				Contact Us | About The GigHub Crew
 			</div>
 		</div>
 	</div>

67b139f3d30d815840fab1c323dbae4a214df815
Dante Conley <danteconley@icloud.com>
2017-03-09 13:53:14 -0700

    added comments to splash page.

--- epic/frontend-sandbox/splash.php
+++ epic/frontend-sandbox/splash.php
@@ -1,23 +1,29 @@
-<?php require_once("lib/head-utils.php");?>
+<?php require_once("lib/head-utils.php"); ?>
 
 <body>
 
+	<!-------Make the login buttons work---->
 	<div id="fb-root"></div>
 	<script>(function(d, s, id) {
 			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) return;
-			js = d.createElement(s); js.id = id;
+			if(d.getElementById(id)) return;
+			js = d.createElement(s);
+			js.id = id;
 			js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=1878867439052171";
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));</script>
 
+	<!-------GigHub Logo------->
 	<div class="container center-splash">
-	<img src="../images/GigHubWhite.png" class="img-responsive center-block">
+		<img src="../images/GigHubWhite.png" class="img-responsive center-block">
 
-				<div class="fb-login-button visible-md-inline visible-lg-inline" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="false"></div>
+		<!-------Button for desktop (FB login)------->
+		<div class="fb-login-button visible-md-inline visible-lg-inline" data-max-rows="1" data-size="xlarge"
+			  data-show-faces="false" data-auto-logout-link="false"></div><br>
 
-
-		<div class="fb-login-button visible-xs-inline visible-sm-inline" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
+		<!-------Button for mobile (FB login)------->
+		<div class="fb-login-button visible-xs-inline visible-sm-inline" data-max-rows="1" data-size="medium"
+			  data-show-faces="false" data-auto-logout-link="false"></div>
 	</div>
 
 </body>

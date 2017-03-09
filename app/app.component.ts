261dde627d1e7d8954e6268f0e52c49588f8d145
Mason Crane <masonecrane@gmail.com>
2017-03-09 10:56:26 -0700

    added navbar view component, template, updated route

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,10 +2,10 @@ import {Component} from "@angular/core";
 
 @Component({
 	// Update selector with YOUR_APP_NAME-app. This needs to match the custom tag in webpack/index.php
-	selector: 'angular2-example-app',
+	selector: 'gighub-app',
 
 	// templateUrl path to your public_html/templates directory.
-	templateUrl: './templates/angular2-example-app.php'
+	templateUrl: './templates/gighub-app.php'
 })
 
 export class AppComponent {}
---
+++ app/components/mainnav-component.ts
@@ -0,0 +1,9 @@
+// special thanks to Rochelle Lewis https://github.com/rlewis2892/meow-app-2/
+import {Component} from "@angular/core";
+
+@Component({
+	selector: "main-nav",
+	templateUrl: "./templates/mainnav.php"
+})
+
+export class MainNavComponent { }

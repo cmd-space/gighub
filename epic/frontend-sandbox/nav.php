fb701894bed6bb3881b261b3dd7cb5db436ab1ac
Brandon Steider <bsteider@cnm.edu>
2017-03-10 13:34:42 -0700

    fixing logo https://app.asana.com/0/256424297438824/285635862591173

--- epic/frontend-sandbox/nav.php
+++ epic/frontend-sandbox/nav.php
@@ -9,30 +9,32 @@
 						<span class="icon-bar"></span>
 					</button>
 				</div>
+				<div class="logo">
+					<div class="col-xs-12 text-center">
+						<img class="logo-image" src="../images/logowhitehorizontal.png"/>
 
+					</div>
+				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 					<ul class="nav navbar-nav">
 
-						<li class="active"><a href="#">Profile<span class="sr-only">(current)</span></a></li>
-						<li><a href="#">Feed</a></li>
+						<li class="active links"><a href="#">Profile<span class="sr-only">(current)</span></a></li>
+						<li class="active links"><a  href="#">Feed</a></li>
+
 					</ul>
 
-						<div class="logo">
-					<div class="col-sm-12 text-center">
-					<img src="../images/logowhitehorizontal.png" height="100"/>
-					</div>
-						</div>
+
 
 					<div class="formandsignout">
 					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Sign Out</a></li>
+						<li class="active links"><a href="#">Sign Out</a></li>
 					</ul>
 					<form class="navbar-form navbar-right">
 						<div class="form-group">
 							<input type="text" class="form-control" placeholder="search no work fuk off m8">
 						</div>
-						<button type="submit" class="btn btn-default">Submit</button>
+						<button type="submit" class="btn btn-default active links">Submit</button>
 					</form>
 				</div
 			</div>><!-- /.navbar-collapse -->

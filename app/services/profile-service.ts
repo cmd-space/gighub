ee37f8f20945a94114ac824866de480a4b5bc730
Mason Crane <masonecrane@gmail.com>
2017-03-10 22:51:06 -0700

    created profile-service and commented out import on venue and profile in post-service

---
+++ app/services/profile-service.ts
@@ -0,0 +1,63 @@
+import {Injectable} from "@angular/core";
+import {Http} from "@angular/http";
+import {Observable} from "rxjs/Observable";
+import {BaseService} from "./base-service";
+import {Profile} from "../classes/profile";
+import {Status} from "../classes/status";
+
+@Injectable()
+export class PostService extends BaseService {
+	constructor(protected http: Http) {
+		super(http);
+	}
+
+	private profileUrl = "api/profile/";
+
+	getProfileByProfileId(profileId: number) : Observable<Profile> {
+		return (this.http.get(this.profileUrl + profileId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getProfileByProfileOAuthId(profileOAuthId: number) : Observable<Profile> {
+		return (this.http.get(this.profileUrl + profileOAuthId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getProfileByProfileTypeId(profileTypeId: number) : Observable<Profile> {
+		return (this.http.get(this.profileUrl + profileTypeId)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getProfileByLocationContent(profileLocation: string) : Observable<Profile> {
+		return (this.http.get(this.profileUrl + profileLocation)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getProfileBySoundCloudUser(profileSoundCloudUser: string) : Observable<Profile> {
+		return (this.http.get(this.profileUrl + profileSoundCloudUser)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	getProfileByProfileUserName(profileUserName: string) : Observable<Profile> {
+		return (this.http.get(this.profileUrl + profileUserName)
+			.map(this.extractData)
+			.catch(this.handleError));
+	}
+
+	putProfile(profile: Profile) : Observable<Profile>{
+		return(this.http.put(this.profileUrl + profile.profileId, profile)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+
+	}
+	deletePost(profile: Profile) : Observable<Profile>{
+		return(this.http.delete(this.profileUrl + profile.profileId)
+			.map(this.extractMessage)
+			.catch(this.handleError));
+	}
+}

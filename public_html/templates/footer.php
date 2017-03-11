f4fb08d909d14c8789362dee858e05a7c801d9af
Dante Conley <danteconley@icloud.com>
2017-03-11 14:45:57 -0700

    added footer.php template.

---
+++ public_html/templates/footer.php
@@ -0,0 +1,14 @@
+<footer class="navbar-fixed-bottom">
+	<div class="container-fluid" id="footer">
+		<div class="row">
+
+			<div class="col-sm-6 text-left-sm">
+				<img src="../images/gighubdude.png" height="40"/>
+			</div>
+
+			<div class="col-md-6 c text-right-sm ">
+				Contact Us | About The GigHub Crew
+			</div>
+		</div>
+	</div>
+</footer>

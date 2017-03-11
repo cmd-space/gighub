cb4a889ebb6c769faa2d8e1a586944b52f345150
Mason Crane <masonecrane@gmail.com>
2017-03-10 23:32:37 -0700

    added about view component, template, updated route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,11 +2,13 @@ import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {MainNavComponent} from "./components/mainnav-component";
 import {NotFoundComponent} from "./components/notfound-component";
+import {AboutComponent} from "./components/about-component";
 
-export const allAppComponents = [HomeComponent, MainNavComponent, NotFoundComponent];
+export const allAppComponents = [HomeComponent, MainNavComponent, AboutComponent, NotFoundComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
+	{path: "about", component: AboutComponent},
 	{path: "**", component: NotFoundComponent}
 ];
 
---
+++ public_html/templates/about.php
@@ -0,0 +1,19 @@
+<main-nav></main-nav>
+
+<main class="bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>About GigHub</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<p>Will be filled with neato stuff.</p>
+			</div>
+			<div class="col-md-6">
+				<p>Eventually</p>
+			</div>
+		</div>
+	</div>
+</main>

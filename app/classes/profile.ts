ebd3a386832bcdeed6a264b6c2951a99775f70e7
Mason Crane <masonecrane@gmail.com>
2017-03-10 21:54:45 -0700

    added profile class

---
+++ app/classes/profile.ts
@@ -0,0 +1,3 @@
+export class Profile {
+	constructor(public profileId: number, public profileOAuthId: number, public profileTypeId: number, public profileBio: string, public profileImageCloudinaryId: string, public profileLocation: string, public profileOAuthToken: string, public profileSoundCloudUser: string, public profileUserName: string){}
+}

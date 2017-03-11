6c50642fa403887b478f56c46d50099d1c8d51b1
Mason Crane <masonecrane@gmail.com>
2017-03-10 23:27:39 -0700

    added notfound view component, template, updated route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,11 +1,13 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
 import {MainNavComponent} from "./components/mainnav-component";
+import {NotFoundComponent} from "./components/notfound-component";
 
-export const allAppComponents = [HomeComponent, MainNavComponent];
+export const allAppComponents = [HomeComponent, MainNavComponent, NotFoundComponent];
 
 export const routes: Routes = [
-	{path: "", component: HomeComponent}
+	{path: "", component: HomeComponent},
+	{path: "**", component: NotFoundComponent}
 ];
 
 export const appRoutingProviders: any[] = [];
---
+++ public_html/templates/notfound.php
@@ -0,0 +1,11 @@
+<main-nav></main-nav>
+
+<main class="bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 text-center">
+				<h1>404 Dakota Not Found.</h1>
+			</div>
+		</div>
+	</div>
+</main>

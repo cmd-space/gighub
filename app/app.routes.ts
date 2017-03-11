f0287564416f982f01ae30127a23b7dd87748930
Mason Crane <masonecrane@gmail.com>
2017-03-10 23:35:33 -0700

    added contact view component, template, updated route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -3,12 +3,14 @@ import {HomeComponent} from "./components/home-component";
 import {MainNavComponent} from "./components/mainnav-component";
 import {NotFoundComponent} from "./components/notfound-component";
 import {AboutComponent} from "./components/about-component";
+import {ContactComponent} from "./components/contact-component";
 
-export const allAppComponents = [HomeComponent, MainNavComponent, AboutComponent, NotFoundComponent];
+export const allAppComponents = [HomeComponent, MainNavComponent, AboutComponent, ContactComponent, NotFoundComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeComponent},
 	{path: "about", component: AboutComponent},
+	{path: "contact", component: ContactComponent},
 	{path: "**", component: NotFoundComponent}
 ];
 
---
+++ public_html/templates/contact.php
@@ -0,0 +1,61 @@
+// this is compliments of Rochelle Lewis https://github.com/rlewis2892/meow-app-2/blob/master/public_html/templates/signup.php
+// This is all filler for now
+<main-nav></main-nav>
+
+<main class="bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Sign Up Here</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<p>Sniff other cat's butt and hang jaw half open thereafter. Sun bathe soft kitty warm kitty little ball of furr stretch, or lick the curtain just to be annoying for pooping rainbow while flying in a toasted bread costume in space sit in box for eats owners hair then claws head.</p>
+			</div>
+			<div class="col-md-6">
+				<!--Begin Contact Form-->
+				<form id="contact-form">
+					<div class="form-group">
+						<label for="name">Name <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-user" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email">Email <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+							<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="pass">Password <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-lock" aria-hidden="true"></i>
+							</div>
+							<input type="password" class="form-control" id="pass" name="pass" placeholder="Name">
+						</div>
+					</div>
+
+					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+					<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+				</form>
+
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>

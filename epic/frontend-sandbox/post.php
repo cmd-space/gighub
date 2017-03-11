78aa6014ccfff6a318e7486daca0a43cd19d5255
Joseph Ramirez <jramirez98@cnm.edu>
2017-03-11 10:51:44 -0700

    added the post.php contents

--- epic/frontend-sandbox/post.php
+++ epic/frontend-sandbox/post.php
@@ -1,61 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
-		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-		<!--		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-		<title>GigHub</title>
-	</head>
-	<body>
-		<div class="container">
-		<header>
-			<h1>Post Create View</h1>
-		</header>
-
-		<div class="container">
-		<!-- Button trigger modal -->
-			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-				Launch demo modal
-			</button>
-
-			<!-- Modal -->
-			<button type="button" data-toggle="modal" data-target="#myModal">Launch modal</button>
-			<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-				<div class="modal-dialog" role="document">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-								<span aria-hidden="true">&times;</span>
-							</button>
-						</div>
-						<div class="modal-body">
-						</div>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-							<button type="button" class="btn btn-primary">Save changes</button>
-						</div>
-					</div>
+<?php require_once("lib/head-utils.php"); ?>
+<body>
+	<?php require_once("nav.php"); ?>
+	<!-- Button trigger modal -->
+	<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#editProfileModal">
+		Launch Post modal
+	</button>
+	<div class="modal fade" tabindex="-1" role="dialog" id="editProfileModal">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title">Create Post</h4>
 				</div>
-			</div>
-	</body>
-
-	<script>
-
-	</script>
+				<div class="modal-body">
+					<form action="#">
+						<div class="form-group">
+							<label for="inputFile">Profile Image</label>
+							<input type="file" class="form-control" id="inputFile">
+							<p class="help-block">Choose your post image</p>
+						</div>
+						<div class="form-group">
+							<label for="postTitle">Title</label>
+							<input class="form-control" type="text" id="profileUserName" name="profileUserName" placeholder="current user name here...">
+						</div>
+						<div class="form-group">
+							<label for="eventDescription">Event Description</label>
+							<textarea class="form-control" name="profileBio" id="profileBio" cols="20" rows="7" placeholder="This will become their current profile bio..."></textarea>
+						</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
+</body>
 </html>

69c01653d08c823e4a06bd430c0de3cb732265d5
Joseph Ramirez <jramirez98@cnm.edu>
2017-03-12 16:21:03 -0600

    added the the contents in the post-component.ts

--- app/components/feed-component.ts
+++ app/components/feed-component.ts
@@ -1,7 +1,42 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Post} from "../classes/post";
+import {PostService} from "../services/post-service";
+import {Status} from "../classes/status";
+import "rxjs/add/operator/switchMap";
+
 
 @Component({
 	templateUrl: "./templates/feed.php"
 })
 
-export class FeedComponent { }
+export class FeedComponent implements OnInit {
+	post: Post = new Post(null, null, null, null, null, null, null, null);
+	status: Status = null;
+	post: Posts[] = [];
+
+
+	constructor(
+		private postService: PostService, private route: ActivatedRoute) {
+	}
+
+	ngOnInit(): void {
+		this.getPostByPostId();
+		this.getAllPosts();
+	}
+
+	getPostByPostId() : void {
+		this.route.params
+			.switchMap((params : Params) => this.postService.getPostByPostId(+params["postId"]))
+			.subscribe(reply => this.post = reply);
+	}
+
+	//ngOnInit(): void {
+	//	this.getAllPosts();
+	//}
+
+	getAllPosts() : void {
+		this.postService.getAllPosts()
+			.subscribe(posts => this.post = posts);
+	}
+}

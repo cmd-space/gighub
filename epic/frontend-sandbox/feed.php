9bde42f4d4f4f628f34f948d3d87bcb852f269bb
Joseph Ramirez <jramirez98@cnm.edu>
2017-03-12 13:52:48 -0600

    created the post-component.ts

---
+++ epic/frontend-sandbox/feed.php
@@ -0,0 +1,61 @@
+<?php require_once("lib/head-utils.php"); ?>
+/**
+ * Created by PhpStorm.
+ * User: josephramirez
+ * Date: 3/11/17
+ * Time: 2:30 PM
+ */
+<body>
+	<?php require_once("nav.php"); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 pull-left">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Post Title</h3>
+					</div>
+						<div class="panel-body">
+						<a class="thumbnail">
+						<img src="" alt="">
+						</a>
+						Feed content
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 pull-left">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Post Title</h3>
+					</div>
+					<div class="panel-body">
+						<a class="thumbnail">
+							<img src="" alt="">
+						</a>
+						Feed content
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 pull-left">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title">Post Title</h3>
+					</div>
+					<div class="panel-body">
+						<a class="thumbnail">
+							<img src="" alt="">
+						</a>
+						Feed content
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>

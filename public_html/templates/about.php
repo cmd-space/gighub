e95475ecb188504406e6e236db788db3ee793180
Dante Conley <danteconley@icloud.com>
2017-03-13 10:12:21 -0600

    small change to see if I can get angular to load or react at all on the web page.

--- public_html/templates/about.php
+++ public_html/templates/about.php
@@ -4,12 +4,12 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<h1>About GigHub</h1>
+				<h1>The Crew</h1>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-6">
-				<p>Will be filled with neato stuff.</p>
+				<p>Bra</p>
 			</div>
 			<div class="col-md-6">
 				<p>Eventually</p>

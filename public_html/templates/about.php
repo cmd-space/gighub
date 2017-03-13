8e7de46425197c892895590250fd7922b5f5f0b4
Dante Conley <danteconley@icloud.com>
2017-03-13 10:32:11 -0600

    Editing "About" page

--- public_html/templates/about.php
+++ public_html/templates/about.php
@@ -9,10 +9,38 @@
 		</div>
 		<div class="row">
 			<div class="col-md-6">
-				<p>Bra</p>
+				<p><strong>Brandon Steider</strong></p>
 			</div>
 			<div class="col-md-6">
-				<p>Eventually</p>
+				<p>I'm Brandon, one of the founders of GigHub, Me and my friend Kyle created the idea about a year ago but didn't have any way to actually make it come to life. I've worked in sales and customer service, and am now a Junior Web Developer. On the side I like to dirtbike, skate, and write. I'm excited to see what the future holds for the GigHub Boys.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<p><strong>Dante Conley</strong></p>
+			</div>
+			<div class="col-md-6">
+				<p>My name’s Dante. I’ve always been interested in the music scene. Whether its organizing an event, playing a show, or just attending a concert. I love it. I’ve worked as a promoter and sound engineer for most of my adult life. Working on GigHub has been a blast. During the project I got to merge two of my favorite things: Music and Technology. Although in its current state it is small, I believe GigHub holds the potential to become a staple in the music industry for fans, promoters, venues, and artists alike.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<p><strong>Joesph Ramirez</strong></p>
+			</div>
+			<div class="col-md-6">
+				<p>Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<p><strong>Mason Crane</strong></p>
+			</div>
+			<div class="col-md-6">
+				<p>Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.Filler...Lots and lots of filler.
+				</p>
 			</div>
 		</div>
 	</div>

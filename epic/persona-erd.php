ba8f63a09eac98ac098f48cd901d85e9f97a70a0
Dante Conley <danteconley@icloud.com>
2017-03-14 14:00:45 -0600

    added initial draft of personas

---
+++ epic/persona-erd.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>GigHub Persona's & ERD</title>
+	</head>
+
+	<body>
+
+		<header>
+			<h1>Persona's:</h1>
+		</header>
+			<ul>
+				<li>
+					<strong>Venues :</strong> A venue would utilize our site for two main reasons. First off, the feed makes
+					it easy for them to advertise concerts and events coming up. Secondly we provide a platform for venues to
+					find artists to fill empty slots or find new talent to perform.
+				</li>
+				<li>
+					<strong>Artists :</strong> As an artist, It is imperative that you promote yourself. Our site will
+					provide an outlet for just that. As more updates come to GigHub, artists will soon be able to make posts
+					on the feed and even message venues and fans. Artists would use our site to jockey for gigs and promote
+					shows they already have booked.
+				</li>
+				<li>
+					<strong>Fans :</strong> As of right now a fan is mostly a spectator. It is a place you can log on to to
+					see what is going on in an area. Functionality includes the ability to search venues by City, Zip, and
+					even Address. When messaging goes live GigHub will turn into a social community.
+				</li>
+			</ul>
+
+
+
+	</body>
+</html>

1f4c408b45c2c9b5b24529fea91c41fc744ae648
Dante Conley <danteconley@icloud.com>
2017-03-14 15:22:17 -0600

    formatting gighub intro page

--- epic/persona-erd.php
+++ epic/persona-erd.php
@@ -6,37 +6,64 @@
 				content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>GigHub Persona's & ERD</title>
+
+
+
+		<style>
+			body {
+				background-color: #252b37
+			}
+
+			h1 {
+				background-color: #252b37;
+				color: white;
+			}
+
+			ul {
+				background-color: #252b37;
+				color: white;
+			}
+		</style>
 	</head>
 
 	<body>
 		<main>
-			<img src="../public_html/images/GigHubWhite.png" alt="GigHub">
+			<center><img src="../public_html/images/GigHubWhite.png" alt="GigHub">
+				<br><br><br><br><br>
 
-			<h1>Persona's:</h1>
+				<h1>Persona's:</h1>
+			</center>
 			<ul>
 				<li>
-					<strong>Venues :</strong> A venue would utilize our site for two main reasons. First off, the feed makes
+					<strong>Venues :</strong> A venue would wangt to utilize our site for two main reasons. First off, the
+					feed makes
 					it easy for them to advertise concerts and events coming up. Secondly we provide a platform for venues to
 					find artists to fill empty slots or find new talent to perform.
 				</li>
+				<br>
 				<li>
 					<strong>Artists :</strong> As an artist, It is imperative that you promote yourself. Our site will
 					provide an outlet for just that. As more updates come to GigHub, artists will soon be able to make posts
 					on the feed and even message venues and fans. Artists would use our site to jockey for gigs and promote
 					shows they already have booked.
 				</li>
+				<br>
 				<li>
 					<strong>Fans :</strong> As of right now a fan is mostly a spectator. It is a place you can log on to to
-					see what is going on in an area. Functionality includes the ability to search venues by Name, City, Zip, and
+					see what is going on in an area. Functionality includes the ability to search venues by Name, City, Zip,
+					and
 					even Address. When messaging goes live GigHub will start to evolve into a social community.
 				</li>
 			</ul>
+			<center>
+				<br><br>
+
+				<h1>ERD:</h1>
+				<img src="../public_html/images/gighub copy.svg" alt="GigHub ERD">
+			</center>
 		</main>
-
-		<h1>ERD:</h1>
-		<img src="../public_html/images/gighub copy.svg" alt="GigHub ERD">
-
-
 
 	</body>
 </html>
+
+

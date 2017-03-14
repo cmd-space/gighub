a1f0e71879f6c7593904a51fd2f0bbf8aae89d2d
Brandon Steider <bsteider@cnm.edu>
2017-03-14 11:33:12 -0600

    editing view post

--- public_html/templates/viewpost.php
+++ public_html/templates/viewpost.php
@@ -9,63 +9,63 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 	<div class="row">
 		<div class="col-md-3"></div>
 		<div class="col-md-6">
-			<h1 class="text-center">{{ post.postContent }}</h1>
+			<h1 class="text-center">{{ post.postTitle }}</h1>
 			<img src="https://www.fillmurray.com/140/200" alt="ALL the BMs" class="img-responsive center-block">
-			<h2 class="text-center">{{ post.postCreatedDate }}</h2>
+			<p class="text-center">{{ post.postImage }}</p>
+			<h2 class="text-center">{{ post.postContent }}</h2>
 			<p class="text-center">{{ post.postEventDate }}</p>
 		</div>
 	</div>
 <?php
-if(empty($_SESSION['profile']) === false) {
+if(empty($_SESSION['post']) === false) {
 	?>
 	<div class="row">
 		<div class="col-xs-12">
-			<h1 class="text-center">Editing {{ profile.profileUserName }}</h1>
+			<h1 class="text-center">Editing {{ post.postContent }}</h1>
 		</div>
 	</div>
 
 	<div class="row">
 		<div class="col-md-4"></div>
 		<div class="col-md-4">
-			<form #profileForm="ngForm" name="profileForm" id="profileForm" (ngSubmit)="putProfile();" novalidate>
+			<form #profileForm="ngForm" name="postForm" id="postForm" (ngSubmit)="putPost();" novalidate>
 				<div class="form-group">
-					<label for="inputFile">Profile Image</label>
+					<label for="inputFile">Post Image</label>
 					<input type="file" class="form-control" name="inputFile" id="inputFile">
-					<p class="help-block">Choose your profile image</p>
+					<p class="help-block">Choose your post image</p>
 				</div>
 				<div class="form-group">
-					<label for="profileType">Profile Type</label>
-					<select name="profileType" class="form-control" id="profileType" [(ngModel)]="profile.profileTypeId" #profileType="ngModel" required>
-						<option [value]="1">Fan</option>
+					<label for="postTitle">Post Title</label>
+					<select name="postTitle" class="form-control" id="postTitle" [(ngModel)]="post.postTitleId" #postTitle="ngModel" required>
 						<option [value]="2">Artist</option>
 						<option [value]="3">Venue</option>
 					</select>
-					<div [hidden]="profileType.valid || profileType.pristine" class="alert alert-danger" role="alert">
-						<p *ngIf="profileType.errors?.required">Bloop</p>
+					<div [hidden]="postTitle.valid || postTitle.pristine" class="alert alert-danger" role="alert">
+						<p *ngIf="postTitle.errors?.required">Bloop</p>
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="profileUserName">Profile User Name</label>
-					<input class="form-control" type="text" id="profileUserName" name="profileUserName"
-							 placeholder="current user name here..." [(ngModel)]="profile.profileUserName" #profileUserName="ngModel" required>
-					<div [hidden]="profileUserName.valid || profileUserName.pristine" class="alert alert-danger" role="alert">
-						<p *ngIf="profileUserName.errors?.required">Bloop</p>
+					<label for="postId">Post Name</label>
+					<input class="form-control" type="text" id="postId" name="postId"
+							 placeholder="current user name here..." [(ngModel)]="post.postId" #postId="ngModel" required>
+					<div [hidden]="postId.valid || postId.pristine" class="alert alert-danger" role="alert">
+						<p *ngIf="postId.errors?.required">Bloop</p>
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="profileBio">Profile Bio</label>
-					<textarea class="form-control" name="profileBio" id="profileBio" cols="20" rows="7"
-								 placeholder="This will become their current profile bio..." [(ngModel)]="profile.profileBio" #profileBio="ngModel" required></textarea>
-					<div [hidden]="profileBio.valid || profileBio.pristine" class="alert alert-danger" role="alert">
-						<p *ngIf="profileBio.errors?.required">Bloop</p>
+					<label for="postTitle">Post Title</label>
+					<textarea class="form-control" name="postTitle" id="postTitle" cols="20" rows="7"
+								 placeholder="This will become their current post title..." [(ngModel)]="post.postTitle" #profileBio="ngModel" required></textarea>
+					<div [hidden]="postcontent.valid || postContent.pristine" class="alert alert-danger" role="alert">
+						<p *ngIf="postContent.errors?.required">Bloop</p>
 					</div>
 				</div>
 				<div class="form-group">
-					<label for="profileLocation">Location</label>
-					<input class="form-control" type="text" id="profileLocation" name="profileLocation"
-							 placeholder="current user location here..." [(ngModel)]="profile.profileLocation" #profileLocation="ngModel" required>
-					<div [hidden]="profileLocation.valid || profileLocation.pristine" class="alert alert-danger" role="alert">
-						<p *ngIf="profileLocation.errors?.required">Bloop</p>
+					<label for="postEventDate">Event Date</label>
+					<input class="form-control" type="text" id="postEventDate" name="postEventDate"
+							 placeholder="current Event Date Here..." [(ngModel)]="post.postEventDate" #postEventDate="ngModel" required>
+					<div [hidden]="postEventDate.valid || postEventDate.pristine" class="alert alert-danger" role="alert">
+						<p *ngIf="postEventDate.errors?.required">Bloop</p>
 					</div>
 				</div>
 				<div class="form-group">

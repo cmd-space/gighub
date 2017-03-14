8eb2a078176c759062b5fbf74662e165b740f45b
Mason Crane <masonecrane@gmail.com>
2017-03-13 18:10:54 -0600

    pushing

--- public_html/templates/mainnav.php
+++ public_html/templates/mainnav.php
@@ -25,7 +25,7 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
 
-				<li class="active links"><a href="https://bootcamp-coders.cnm.edu/~mcrane2/gighub/public_html/profile/<?php echo $_SESSION['profile']->getProfileId(); ?>">Profile</a></li>
+				<li class="active links"><a href="https://bootcamp-coders.cnm.edu/~mcrane2/gighub/public_html/profile/<?php echo $_SESSION['profile']->getProfileId(); ?>">Profile1</a></li>
 				<li class="active links"><a href="https://bootcamp-coders.cnm.edu/~mcrane2/gighub/public_html/feed">Feed</a></li>
 				<li class="active links"><a href="https://bootcamp-coders.cnm.edu/~mcrane2/gighub/public_html/post">post</a></li>
 

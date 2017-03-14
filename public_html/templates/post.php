f40e48033f90b8eb8e77a9237736c7e943ad64a3
Brandon Steider <bsteider@cnm.edu>
2017-03-14 11:47:15 -0600

    form on view post should be mostly ok

--- public_html/templates/post.php
+++ public_html/templates/post.php
@@ -7,19 +7,28 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 
 <form action="#">
 	<div class="form-group">
-		<label for="inputFile">Post Image</label>
-		<input type="file" class="form-control" id="inputFile">
-		<p class="help-block">Choose your post image</p>
-	</div>
-	<div class="form-group">
 		<label for="postTitle">Post Title</label>
 		<input class="form-control" type="text" id="profileUserName" name="profileUserName"
 				 placeholder="current user name here...">
 	</div>
+
+	<div class="form-group">
+		<label for="inputFile">Post Image</label>
+		<input type="file" class="form-control" id="inputFile">
+		<p class="help-block">Choose your post image</p>
+	</div>
+
 	<div class="form-group">
 		<label for="eventDescription">Event Description</label>
 		<textarea class="form-control" name="profileBio" id="profileBio" cols="20" rows="7"
 					 placeholder="This will become their current profile bio..."></textarea>
 	</div>
+
+	<div class="form-group">
+		<label for="postEventDate">Event Date</label>
+		<input class="form-control" type="text" id="postEventDate" name="postEventDate"
+				 placeholder="current date here...">
+	</div>
+
 </form>
 

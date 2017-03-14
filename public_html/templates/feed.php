c5aab06ebf7e24cda9c33a235ed7ff3e655d7249
Joseph Ramirez <jramirez98@cnm.edu>
2017-03-14 11:54:31 -0600

    added the content in venue-detail. added content venue profile, venue image block, venue name block, and venue address 1 block.

--- public_html/templates/feed.php
+++ public_html/templates/feed.php
@@ -1,4 +1,5 @@
 <main-nav></main-nav>
+<body>
 <div class="container" id="feed">
 	<div class="row" *ngFor="let post of posts">
 		<div class="col-md-2"></div>
@@ -6,10 +7,11 @@
 			<div class="panel panel-primary">
 				<div class="panel-heading">
 					<h3 class="panel-title">{{ post.postTitle }}</h3>
+					<p></p>
 				</div>
 						<img src="" alt="">
 					Feed content
 				</div>
 			</div>
 		</div>
-	</div>
+	</div></body>

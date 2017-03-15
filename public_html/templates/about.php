f6b0cf0dcf25499be8c3131f041e2b241c1bfd40
Brandon Steider <bsteider@cnm.edu>
2017-03-15 11:32:13 -0600

    moved files

--- public_html/templates/about.php
+++ public_html/templates/about.php
@@ -45,3 +45,61 @@
 		</div>
 	</div>
 </main>
+
+<main class="bg">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1>Contact Us</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-6">
+				<p>Have any questions for the GigHub Team? Get in touch below.</p>
+			</div>
+			<div class="col-md-6">
+				<!--Begin Contact Form-->
+				<form id="contact-form">
+					<div class="form-group">
+						<label for="name">Name <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-user" aria-hidden="true"></i>
+							</div>
+							<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="email">Email <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-envelope" aria-hidden="true"></i>
+							</div>
+							<input type="email" class="form-control" id="email" name="email" placeholder="Email">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="pass">Password <span class="text-danger">*</span></label>
+						<div class="input-group">
+							<div class="input-group-addon">
+								<i class="fa fa-lock" aria-hidden="true"></i>
+							</div>
+							<input type="password" class="form-control" id="pass" name="pass" placeholder="Name">
+						</div>
+					</div>
+
+					<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Send</button>
+					<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+				</form>
+
+				<!--empty area for form error/success output-->
+				<div class="row">
+					<div class="col-xs-12">
+						<div id="output-area"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</main>

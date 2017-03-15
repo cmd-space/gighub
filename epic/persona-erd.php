c2c6a0b6e4cf12ba8332895b8f638405c117af5a
Dante Conley <danteconley@icloud.com>
2017-03-15 11:07:31 -0600

    tweaking persona erd page

--- epic/persona-erd.php
+++ epic/persona-erd.php
@@ -5,25 +5,28 @@
 		<meta name="viewport"
 				content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
-		<title>GigHub Persona's & ERD</title>
+		<title>Welcome to GigHub!</title>
 
 		<style>
-			body {
+			body {font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+				font-size: 20px;
 				background-color: #252b37
 			}
 
-			h1 {
+			h1 {font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+				font-size: 30px;
 				background-color: #252b37;
 				color: white;
 			}
 
-			ul {
+			ul {font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+				font-size: 20px;
 				background-color: #252b37;
 				color: white;
 				padding: 0 20%;
 			}
 
-			a {
+			a {font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
 				background-color: #252b37;
 				color: white;
 			}
@@ -31,36 +34,35 @@
 	</head>
 
 	<body>
+		<br><br>
 		<main>
 			<center><img src="../public_html/images/GigHubWhite.png" alt="GigHub">
-				<br><br><br><br><br>
+				<br><br><br><br><br><br><br><br><br><br>
 
 				<h1>Persona's:</h1>
 			</center>
 			<ul>
 				<li>
-					<strong>Venues :</strong> A venue would wangt to utilize our site for two main reasons. First off, the
-					feed makes
-					it easy for them to advertise concerts and events coming up. Secondly we provide a platform for venues to
-					find artists to fill empty slots or find new talent to perform.
+					<strong>Venues :</strong> A venue would want to utilize our site for two main reasons. First off, the
+					feed makes it easy for them to advertise concerts and events coming up at their space. Secondly, we
+					provide a platform for venues to find artists to fill empty slots, or find new talent to perform.
 				</li>
 				<br>
 				<li>
 					<strong>Artists :</strong> As an artist, It is imperative that you promote yourself. Our site will
 					provide an outlet for just that. As more updates come to GigHub, artists will soon be able to make posts
-					on the feed and even message venues and fans. Artists would use our site to jockey for gigs and promote
-					shows they already have booked.
+					on the feed and even message venues and fans. Artists would use our site to jockey for gigs, promote
+					shows they already have booked, and advertise their music to fans via a SoundCloud link on their profile.
 				</li>
 				<br>
 				<li>
-					<strong>Fans :</strong> As of right now a fan is mostly a spectator. It is a place you can log on to to
-					see what is going on in an area. Functionality includes the ability to search venues by Name, City, Zip,
-					and
-					even Address. When messaging goes live GigHub will start to evolve into a social community.
+					<strong>Fans :</strong> As of right now, a fan is a spectator. They can log on and view a feed displaying
+					all the gigs logged into the database. When messaging goes live, GigHub will evolve into a social
+					community.
 				</li>
 			</ul>
 			<center>
-				<br><br>
+				<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 				<center>
 					<h1>ERD:</h1>
@@ -70,7 +72,7 @@
 
 				<center>
 					<a href="url">
-						<strong><big>Activate GigHub!</big></strong>
+						<strong><big><h1>Activate GigHub!</h1></big></strong>
 					</a>
 				</center>
 		</main>
